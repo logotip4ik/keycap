08784f9f769b18b134838c70d45e730ec94aa920
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-11-18 01:08:34 +0200

    feat: fetch and show note content at note route

--- pages/@[user]/[...folders]/[note].vue
+++ pages/@[user]/[...folders]/[note].vue
@@ -1,8 +1,25 @@
-<template>
-  <div>contents of the note {{ $route.params.note }}
+<script setup lang="ts">
+import type { Note } from '@prisma/client';
 
-    <pre>
-      {{ JSON.stringify($route.params) }}
-    </pre>
+const { params } = useRoute();
+const notesCache = useNotesCache();
+const { data: note, pending } = useLazyFetch<Note>(() => `/api/note/${params.note}`, {
+  server: false,
+  default: () => notesCache.get(params.id) || null,
+  onResponse: ({ response }) => {
+    if (!response._data?.id) return;
+
+    notesCache.set(response._data.id, response._data as Note);
+  },
+});
+</script>
+
+<template>
+  <div v-if="note">
+    {{ note.name }}
+    {{ note.content }}
+  </div>
+  <div v-else-if="pending">
+    loading placeholder
   </div>
 </template>
