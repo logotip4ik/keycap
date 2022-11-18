c185bf7ee3310741f3b654ffe70f0fe88ffc2971
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-11-19 00:48:01 +0200

    feat: add update note handler

--- server/api/note/[...path].ts
+++ server/api/note/[...path].ts
@@ -1,5 +1,7 @@
 import { createError, getRouterParam, isMethod, sendError } from 'h3';
 
+import type { Note } from '@prisma/client';
+
 import getPrisma from '~/prisma';
 import { getUserFromEvent } from '~/server/utils/auth';
 import { generateNotePath } from '~/server/utils';
@@ -20,6 +22,36 @@ export default defineEventHandler(async (event) => {
     });
 
     return note || {};
+  }
+
+  if (isMethod(event, 'PUT')) {
+    interface UpdatableFields { name?: string; content?: string }
+
+    const whitelistedFieldUpdates: Array<'name' | 'content'> = ['name', 'content'];
+    const fieldsToUpdate = await readBody<UpdatableFields>(event);
+    const query = getQuery(event);
+
+    const data: UpdatableFields = {};
+
+    for (const field of whitelistedFieldUpdates)
+      if (fieldsToUpdate[field]) data[field] = fieldsToUpdate[field];
+
+    let updatedNote: Omit<Note, 'ownerId' | 'parentId'>;
+
+    try {
+      updatedNote = await prisma.note.update({
+        data,
+        where: { path: generateNotePath(user.username, notePath) },
+        select: { id: true, name: true, content: true, path: true, updatedAt: true, createdAt: true },
+      });
+    }
+    catch {
+      if (query.getNote === 'true') return {};
+      return { stats: 'error' };
+    }
+
+    if (query.getNote === 'true') return updatedNote || {};
+    return { status: 'ok' };
   }
 
   return {};
