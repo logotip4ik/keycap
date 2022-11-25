6b0df7f707f8b8dadcab5c29d350be8534edf4b2
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-11-25 19:42:10 +0200

    feat: add delete method and update get and put folder methods

--- server/api/folder/[...path].ts
+++ server/api/folder/[...path].ts
@@ -1,21 +1,18 @@
-import { createError, getRouterParams, isMethod, sendError } from 'h3';
+import { createError, getRouterParam, isMethod, sendError } from 'h3';
 
 import { getUserFromEvent } from '~/server/utils/auth';
-import { generateFolderPath } from '~/server/utils';
+import { generateFolderPath, toBigInt } from '~/server/utils';
 import getPrisma from '~/prisma';
 
 export default defineEventHandler(async (event) => {
-  const params: { path?: string } = getRouterParams(event) || {};
-
-  if (!params?.path) params.path = '/';
-
   const user = await getUserFromEvent(event);
 
   if (!user) return sendError(event, createError({ status: 401 }));
 
   const prisma = getPrisma();
 
-  const folderPath = generateFolderPath(user.username, params.path);
+  const path = getRouterParam(event, 'path');
+  const folderPath = generateFolderPath(user.username, path);
 
   if (isMethod(event, 'GET')) {
     const folder = await prisma.folder.findFirst({
@@ -35,15 +32,61 @@ export default defineEventHandler(async (event) => {
         },
 
         subfolders: {
-          select: { id: true, name: true, path: true, updatedAt: true, createdAt: true },
+          select: { id: true, name: true, root: true, path: true, updatedAt: true, createdAt: true },
           orderBy: { name: 'asc' },
         },
       },
     });
 
-    if (!folder) sendError(event, createError({ status: 404 }));
+    if (!folder) return sendError(event, createError({ status: 404 }));
 
     return folder;
+  }
+
+  if (isMethod(event, 'POST')) {
+    interface CreateFolderProps { name: string; parentId: string }
+
+    const body = await readBody<CreateFolderProps>(event);
+
+    try {
+      const folder = await prisma.folder.create({
+        data: {
+          name: body.name,
+          path: folderPath,
+          owner: { connect: { email: user.email } },
+          parent: { connect: { id: toBigInt(body.parentId) } },
+        },
+
+        select: {
+          id: true,
+          name: true,
+          path: true,
+          root: true,
+
+          updatedAt: true,
+          createdAt: true,
+
+          notes: true,
+          subfolders: true,
+        },
+      });
+
+      return folder;
+    }
+    catch {
+      return sendError(event, createError({ statusCode: 500 }));
+    }
+  }
+
+  if (isMethod(event, 'DELETE')) {
+    try {
+      await prisma.folder.delete({ where: { path: folderPath } });
+
+      return { status: 'ok' };
+    }
+    catch (error) {
+      return sendError(event, createError({ status: 500 }));
+    }
   }
 
   return { };
