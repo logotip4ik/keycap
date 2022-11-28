d753e9de7cc12f0d2fe159dc23d615d1a8a3be0f
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-11-28 23:21:11 +0200

    feat: add basic bubble menu

--- components/workspace/NoteEditor.vue
+++ components/workspace/NoteEditor.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-import { EditorContent, useEditor } from '@tiptap/vue-3';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/shift-away.css';
+
+import { BubbleMenu, EditorContent, useEditor } from '@tiptap/vue-3';
+
 import StarterKir from '@tiptap/starter-kit';
 import Link from '@tiptap/extension-link';
 import Placeholder from '@tiptap/extension-placeholder';
 import TaskList from '@tiptap/extension-task-list';
 import TaskItem from '@tiptap/extension-task-item';
+import BubbleMenuPlugin from '@tiptap/extension-bubble-menu';
 
 interface Props { content: string }
 interface Emits { (event: 'update', content: string): void }
@@ -21,6 +26,7 @@ const editor = useEditor({
     Link,
     TaskList,
     TaskItem,
+    BubbleMenuPlugin,
     Placeholder.configure({
       placeholder: ({ editor }) =>
         editor.isEmpty ? 'Start with heading...' : 'Write something...',
@@ -48,11 +54,91 @@ onBeforeRouteUpdate(() => {
 </script>
 
 <template>
-  <EditorContent class="note-editor" :editor="editor" />
+  <div class="note-editor__wrapper">
+    <!-- TODO: separate bubble menu into another file ? -->
+    <BubbleMenu
+      v-if="editor"
+      :editor="editor"
+      :tippy-options="{ duration: [10, 150], offset: [0, 5], animation: 'shift-away', arrow: false }"
+      class="note-editor__bubble-menu"
+    >
+      <button
+        class="note-editor__bubble-menu__button"
+        :class="{ 'note-editor__bubble-menu__button--active': editor.isActive('bold') }"
+        @click="editor!.chain().focus().toggleBold().run()"
+      >
+        <Icon name="ic:baseline-format-bold" />
+      </button>
+      <button
+        class="note-editor__bubble-menu__button"
+        :class="{ 'note-editor__bubble-menu__button--active': editor.isActive('italic') }"
+        @click="editor!.chain().focus().toggleItalic().run()"
+      >
+        <Icon name="ic:baseline-format-italic" />
+      </button>
+      <button
+        class="note-editor__bubble-menu__button"
+        :class="{ 'note-editor__bubble-menu__button--active': editor.isActive('strike') }"
+        @click="editor!.chain().focus().toggleStrike().run()"
+      >
+        <Icon name="ic:baseline-format-strikethrough" />
+      </button>
+    </BubbleMenu>
+    <EditorContent class="note-editor" :editor="editor" />
+  </div>
 </template>
 
 <style lang="scss">
 .note-editor {
+  height: 100%;
+
+  &__wrapper {
+    height: 100%;
+  }
+
+  &__bubble-menu {
+    &__button {
+      --size-basis: 1.75rem;
+
+      & + & {
+        margin-left: 0.5rem;
+      }
+
+      font-size: 1rem;
+      color:hsla(var(--text-color-hsl), 0.5);
+
+      width: var(--size-basis);
+      height: var(--size-basis);
+
+      appearance: none;
+      background-color: transparent;
+      border: 1px solid hsla(var(--text-color-hsl), 0.5);
+      border-radius: .25rem;
+
+      cursor: pointer;
+      transition: color .3s, border .3s, background-color .3s;
+
+      &:is(:hover, :focus) {
+        color: hsla(var(--text-color-hsl), 1);
+
+        border: 1px solid hsla(var(--text-color-hsl), 0.75);
+      }
+
+      &--active {
+        color: hsla(var(--surface-color-hsl), 0.75);
+
+        border: 1px solid transparent;
+        background-color: hsla(var(--text-color-hsl), 0.55);
+
+        &:is(:hover, :focus) {
+          color: hsla(var(--surface-color-hsl), 1);
+
+          background-color: hsla(var(--text-color-hsl), 0.85);
+        }
+      }
+    }
+  }
+
   .ProseMirror {
     font-size: 105%;
 
