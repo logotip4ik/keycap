b80b6ea41d9adb074d25f8318789f2827d8e1df2
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2022-12-09 23:23:23 +0200

    refactor: add some checks before updating note or folder

--- composables/store.ts
+++ composables/store.ts
@@ -36,17 +36,23 @@ export const useFoldersCache = () => foldersCache;
 export function deleteNoteFromFolder(noteToDelete: NoteMinimal, parent: FolderWithContents) {
   const noteIdxToDelete = parent.notes.findIndex((note) => note.id === noteToDelete.id);
 
+  if (noteIdxToDelete === -1) return;
+
   parent.notes.splice(noteIdxToDelete, 1);
 }
 
 export function deleteSubfolderFromFolder(subfolderToDelete: FolderWithContents, parent: FolderWithContents) {
   const folderIdxToDelete = parent.subfolders.findIndex((folder) => folder.id === subfolderToDelete.id);
 
+  if (folderIdxToDelete === -1) return;
+
   parent.subfolders.splice(folderIdxToDelete, 1);
 }
 
 export function updateNoteInFolder(noteToUpdate: NoteMinimal, fieldsToUpdate: Updatable<NoteMinimal>, parent: FolderWithContents) {
   const noteIdxToUpdate = parent.notes.findIndex((note) => note.id === noteToUpdate.id);
+
+  if (noteIdxToUpdate === -1) return;
 
   parent.notes[noteIdxToUpdate] = {
     ...noteToUpdate,
