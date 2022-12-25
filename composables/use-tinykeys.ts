b88a3ef385025c1ceb26063bc8155d12d665e4a0
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2022-12-25 22:22:17 +0200

    refactor: add possibility to pass tinykeys options

--- composables/use-tinykeys.ts
+++ composables/use-tinykeys.ts
@@ -1,10 +1,10 @@
 import tinykeys from 'tinykeys';
 
-import type { KeyBindingMap } from 'tinykeys';
+import type { KeyBindingMap, KeyBindingOptions } from 'tinykeys';
 
-export default (shortcuts: KeyBindingMap) => {
+export default (shortcuts: KeyBindingMap, options: KeyBindingOptions = {}) => {
   onMounted(() => {
-    const unregisterTinykeys = tinykeys(window, shortcuts);
+    const unregisterTinykeys = tinykeys(window, shortcuts, options);
 
     onBeforeUnmount(() => unregisterTinykeys());
   });
