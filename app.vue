b4fb916e8a029cc4edda96e925c43d6770686c03
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2022-12-30 13:58:55 +0200

    refactor: preload font

--- app.vue
+++ app.vue
@@ -1,3 +1,19 @@
+<script setup>
+import MonaSansUrl from '~/assets/fonts/Mona-Sans/Mona-Sans.woff2?url';
+
+useHead({
+  link: [
+    {
+      rel: 'preload',
+      as: 'font',
+      type: 'font/woff2',
+      crossorigin: 'anonymous',
+      href: MonaSansUrl,
+    },
+  ],
+}, { mode: 'server' });
+</script>
+
 <template>
   <div>
     <NuxtLoadingIndicator color="var(--loading-indicator-color)" />
