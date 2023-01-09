3212de1985ac037b6bcc24d05d0b4faf45909869
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-09 18:56:01 +0200

    revert: remove hoisting of isDev variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,75 +6,69 @@ const TWO_DAYS_IN_SECONDS = 60 * 60 * 24 * 2;
 const TWO_DAYS_CACHE = `private, immutable, max-age=${TWO_DAYS_IN_SECONDS}`;
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig(() => {
-  const isDev = process.env.NODE_ENV === 'development';
+export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: { translate: 'no' },
+      title: 'Keycap',
+      meta: [{ name: 'description', content: 'Better notes ❤. Synced between your devices' }],
+    },
+  },
 
-  console.log({ isDev });
+  routeRules: {
+    '/': { prerender: true, headers: { 'Cache-Control': TWO_DAYS_CACHE } },
+    '/about': { prerender: true, headers: { 'Cache-Control': TWO_DAYS_CACHE } },
+    '/login': { static: true, headers: { 'Cache-Control': TWO_DAYS_CACHE } },
+    '/register': { static: true, headers: { 'Cache-Control': TWO_DAYS_CACHE } },
 
-  return {
-    app: {
-      head: {
-        htmlAttrs: { translate: 'no' },
-        title: 'Keycap',
-        meta: [{ name: 'description', content: 'Better notes ❤. Synced between your devices' }],
-      },
+    '/api/**': { cors: true, cache: false },
+    '/_nuxt/**': { headers: { 'Cache-Control': TWO_DAYS_CACHE } },
+  },
+
+  runtimeConfig: {
+    public: {
+      authCookiePrefix: 'auth',
+    },
+  },
+
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxtjs/device',
+    'nuxt-icon',
+    'nuxt-security',
+  ],
+
+  css: [
+    'normalize.css',
+    '~/assets/styles/global.scss',
+    '~/assets/fonts/Mona-Sans/style.css',
+  ],
+
+  sourcemap: process.env.NODE_ENV === 'development',
+
+  vite: {
+    build: {
+      target: browserslistToEsbuild(),
     },
 
-    routeRules: {
-      '/': { prerender: true, headers: { 'Cache-Control': TWO_DAYS_CACHE } },
-      '/about': { prerender: true, headers: { 'Cache-Control': TWO_DAYS_CACHE } },
-      '/login': { static: true, headers: { 'Cache-Control': TWO_DAYS_CACHE } },
-      '/register': { static: true, headers: { 'Cache-Control': TWO_DAYS_CACHE } },
-
-      '/api/**': { cors: true, cache: false },
-      '/_nuxt/**': { headers: { 'Cache-Control': TWO_DAYS_CACHE } },
-    },
-
-    runtimeConfig: {
-      public: {
-        authCookiePrefix: 'auth',
-      },
-    },
-
-    modules: [
-      '@vueuse/nuxt',
-      '@nuxtjs/device',
-      'nuxt-icon',
-      'nuxt-security',
-    ],
-
-    css: [
-      'normalize.css',
-      '~/assets/styles/global.scss',
-      '~/assets/fonts/Mona-Sans/style.css',
-    ],
-
-    sourcemap: isDev,
-
-    vite: {
-      build: {
-        target: browserslistToEsbuild(),
-      },
-
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: [
-              Object.entries(breakpoints).map(([key, value]) => `$breakpoint-${key}: ${value}px;`).join('\n'),
-            ].join('\n'),
-          },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: [
+            Object.entries(breakpoints).map(([key, value]) => `$breakpoint-${key}: ${value}px;`).join('\n'),
+          ].join('\n'),
         },
       },
     },
+  },
 
-    device: {
-      refreshOnResize: true,
-    },
+  device: {
+    refreshOnResize: true,
+  },
 
-    security: {
-      // @see https://github.com/Baroshem/nuxt-security/issues/42#issuecomment-1311727911
-      hidePoweredBy: false,
-      enabled: !isDev,
-    },
-  };
+  security: {
+    // @see https://github.com/Baroshem/nuxt-security/issues/42#issuecomment-1311727911
+    hidePoweredBy: false,
+    enabled: process.env.NODE_ENV !== 'development',
+  },
 });
