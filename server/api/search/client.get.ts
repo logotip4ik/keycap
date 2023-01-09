b42d0dd240f0fff38a921c050907108b32c955f9
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-09 18:05:45 +0200

    chore: install command score

---
+++ server/api/search/client.get.ts
@@ -0,0 +1,44 @@
+import getPrisma from '~/prisma';
+import { getUserFromEvent } from '~/server/utils/auth';
+
+import type { FuzzyItem } from '~~/workers/fuzzy';
+
+export default defineEventHandler(async (event) => {
+  const user = await getUserFromEvent(event);
+
+  if (!user)
+    return sendError(event, createError({ statusCode: 401 }));
+
+  const prisma = getPrisma();
+
+  const query = getQuery(event) || {};
+
+  let skip = 0;
+  let select = 50;
+
+  // @ts-expect-error checking if query param could be param
+  if (query.skip && !isNaN(query.skip))
+    skip = Number(query.skip);
+  // @ts-expect-error checking if query param could be param
+  if (query.select && !isNaN(query.select))
+    select = Number(query.select);
+
+  const pathToSearch = `/${user.username}`;
+
+  const [notes, folders] = await Promise.all([
+    prisma.note.findMany({
+      skip,
+      where: { path: { startsWith: pathToSearch } },
+      take: Math.floor(select / 2),
+      select: { name: true, path: true },
+    }),
+    prisma.folder.findMany({
+      skip,
+      where: { path: { startsWith: pathToSearch } },
+      take: Math.floor(select / 2),
+      select: { name: true, path: true, root: true },
+    }),
+  ]);
+
+  return [...notes, ...folders] as FuzzyItem[];
+});
