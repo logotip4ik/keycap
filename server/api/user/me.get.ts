1c7b29281fabb763adeaf2d99168a16a8ef96c25
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-20 14:58:34 +0200

    refactor: remove auth cookies if no user present

--- server/api/user/me.get.ts
+++ server/api/user/me.get.ts
@@ -1,10 +1,13 @@
-import { getUserFromEvent } from '~/server/utils/auth';
+import { getUserFromEvent, removeAuthCookies } from '~/server/utils/auth';
 
 export default defineEventHandler(async (event) => {
   const user = await getUserFromEvent(event);
 
-  if (!user)
+  if (!user) {
+    await removeAuthCookies(event);
+
     return sendError(event, createError({ statusCode: 401 }));
+  }
 
   return user;
 });
