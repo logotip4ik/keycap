dd2a18a9afd85445dbd7211af25ad78e0b50ae13
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-23 22:46:00 +0200

    refactor: working font preload link

--- app.vue
+++ app.vue
@@ -1,17 +1,25 @@
 <script setup>
+import requrl from 'requrl';
+
 import MonaSansUrl from '~/assets/fonts/Mona-Sans/Mona-Sans.woff2';
 
-useHead({
-  link: [
-    {
-      rel: 'preload',
-      as: 'font',
-      type: 'font/woff2',
-      crossorigin: 'anonymous',
-      href: MonaSansUrl,
-    },
-  ],
-});
+if (process.server) {
+  const event = useRequestEvent();
+
+  const base = requrl(event.node.req);
+
+  useHead({
+    link: [
+      {
+        rel: 'preload',
+        as: 'font',
+        type: 'font/woff2',
+        crossorigin: 'anonymous',
+        href: `${base}${MonaSansUrl}`,
+      },
+    ],
+  });
+}
 </script>
 
 <template>
