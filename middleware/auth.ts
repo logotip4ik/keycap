75ba945145a509619a21f17be74d2464b1142675
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-24 14:50:51 +0200

    refactor: show error if usernames does not match

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,9 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((from) => {
   const user = useUser();
 
   if (!user.value)
     return navigateTo('/login');
+
+  if (decodeURIComponent(user.value.username) !== decodeURIComponent(from.params.user as string))
+    return abortNavigation({ statusCode: 401, statusMessage: 'You are not authorized to access this route' });
 });
