e02f4fe2c506e1b413a670bd1ee5369b055aec4c
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-24 22:13:37 +0200

    fix: prevent showing dublicate slow internet toasts

--- components/workspace/Contents/Contents.vue
+++ components/workspace/Contents/Contents.vue
@@ -40,7 +40,8 @@ const { data: fetchedFolder, error } = useLazyAsyncData<FolderWithContents>(
     return $fetch(`/api/folder/${getApiFolderPath()}`, {
       retry: 2,
       onRequest: () => {
-        loadingToast = createToast('Seems like your internet is slow 🤔', { duration: 999999, delay: 3000, type: 'loading' });
+        if (!loadingToast)
+          loadingToast = createToast('Seems like your internet is slow 🤔', { duration: 999999, delay: 3000, type: 'loading' });
       },
       onResponse: () => {
         loadingToast?.remove();
