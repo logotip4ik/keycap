37c822705051fdf9e6cee8a6f47091dd36021cb1
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-25 22:15:11 +0200

    refactor: wrap font preloading statement into process.server block

--- app.vue
+++ app.vue
@@ -1,21 +1,23 @@
 <script setup>
 import MonaSansUrl from '~/assets/fonts/Mona-Sans/Mona-Sans.woff2';
 
-const localhost = 'localhost:3000';
-const base = process.env.SITE_ORIGIN || localhost;
-const protocol = base === localhost ? 'http://' : 'https://';
+if (process.server) {
+  const localhost = 'localhost:3000';
+  const base = process.env.SITE_ORIGIN || localhost;
+  const protocol = base === localhost ? 'http://' : 'https://';
 
-useHead({
-  link: [
-    {
-      rel: 'preload',
-      as: 'font',
-      type: 'font/woff2',
-      crossorigin: 'anonymous',
-      href: `${protocol}${base}${MonaSansUrl}`,
-    },
-  ],
-});
+  useHead({
+    link: [
+      {
+        rel: 'preload',
+        as: 'font',
+        type: 'font/woff2',
+        crossorigin: 'anonymous',
+        href: `${protocol}${base}${MonaSansUrl}`,
+      },
+    ],
+  });
+}
 </script>
 
 <template>
