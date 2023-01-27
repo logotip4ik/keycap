55a2d2fcbcf967ad51bbb2a19c5d37351bfd5324
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-27 13:18:50 +0200

    feat: auto remove handler on scope dispose

--- composables/use-mitt.ts
+++ composables/use-mitt.ts
@@ -1,3 +1,4 @@
+import type { Emitter } from 'mitt';
 import mitt from 'mitt';
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
@@ -8,6 +9,17 @@ type Events = {
 
 const emitter = mitt<Events>();
 
-export default () => {
-  return { ...emitter };
+export default (): Emitter<Events> => {
+  return {
+    ...emitter,
+    // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
+    // @ts-ignore we still return correct type from function
+    on: (type, handler) => {
+      emitter.on(type, handler);
+
+      onScopeDispose(() => {
+        emitter.off(type, handler);
+      });
+    },
+  };
 };
