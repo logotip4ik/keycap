53b215b1448467d2fe68bb48c45c72b48e83074d
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-27 14:39:10 +0200

    refactor: rework some of folder path resolution functions

--- components/workspace/Contents/Contents.vue
+++ components/workspace/Contents/Contents.vue
@@ -11,34 +11,26 @@ const offlineStorage = useOfflineStorage();
 const { shortcuts } = useAppConfig();
 const mitt = useMitt();
 
-const folder = shallowRef<FolderWithContents | null | undefined>(
-  foldersCache.get(withoutTrailingSlash(`/${route.params.user}/${getApiFolderPath()}`)),
-);
+const folderPath = computed(() => {
+  const folders = Array.isArray(route.params.folders) ? route.params.folders : [route.params.folders];
 
-const folderIdentifier = computed(() => {
-  if (!route.params.folders) return '';
-
-  // this accounts for empty string as well as empty array
-  if (route.params.folders.length === 0) return '';
-
-  return `${route.params.folders.length}-${route.params.folders.at(-1)}`;
+  return withoutTrailingSlash(`/${route.params.user}/${folders.map(encodeURIComponent).join('/')}`);
 });
+
+const folder = shallowRef<FolderWithContents | null>(
+  foldersCache.get(folderPath.value) || null,
+);
 
 let firstTimeFetch = true;
 let loadingToast: undefined | ToastInstance;
 const { data: fetchedFolder, error } = useLazyAsyncData<FolderWithContents>(
   'folder',
   () => {
-    const folders = route.params.folders;
-    const path = Array.isArray(folders) ? folders.at(-1) : folders as string;
+    folder.value = foldersCache.get(folderPath.value) || null;
 
-    const folderPath = withLeadingSlash(route.params.user as string) + withLeadingSlash(path);
-    const newCurrentFolder = foldersCache.get(withoutTrailingSlash(folderPath));
+    const apiFolderPath = folderPath.value.split('/').slice(2).join('/');
 
-    if (newCurrentFolder) folder.value = newCurrentFolder;
-    else folder.value = null;
-
-    return $fetch(`/api/folder/${getApiFolderPath()}`, {
+    return $fetch(`/api/folder/${apiFolderPath}`, {
       retry: 2,
       onRequest: () => {
         if (!loadingToast) {
@@ -55,7 +47,7 @@ const { data: fetchedFolder, error } = useLazyAsyncData<FolderWithContents>(
       },
     });
   },
-  { server: false, watch: [folderIdentifier] },
+  { server: false, watch: [folderPath] },
 );
 
 const mergedContents = computed(() => {
@@ -63,10 +55,6 @@ const mergedContents = computed(() => {
 
   return [...folder.value.notes, ...folder.value.subfolders] as FolderOrNote[];
 });
-
-function getApiFolderPath() {
-  return Array.isArray(route.params.folders) ? route.params.folders.join('/') : '';
-}
 
 async function goUpFolder() {
   const currentFolderPath = folder.value!.path;
@@ -77,7 +65,7 @@ async function goUpFolder() {
 }
 
 mitt.on('cache:populated', () => {
-  folder.value = foldersCache.get(withoutTrailingSlash(`/${route.params.user}/${getApiFolderPath()}`));
+  folder.value = foldersCache.get(folderPath.value) || null;
 });
 
 watch(error, async (error) => {
@@ -95,9 +83,7 @@ watch(error, async (error) => {
     return;
 
   // last chance to show user folder, if iterator in @[user].vue page hasn't yet set the foldersCache
-  const folderPath = withoutTrailingSlash(`/${route.params.user}/${getApiFolderPath()}`);
-
-  const offlineFolder = await offlineStorage.value?.getItem(folderPath) as FolderWithContents;
+  const offlineFolder = await offlineStorage.value?.getItem(folderPath.value) as FolderWithContents;
 
   if (!offlineFolder)
     return createToast('No offline copy found');
@@ -131,81 +117,18 @@ useTinykeys({
 
 <template>
   <Transition name="fade">
-    <template v-if="folder">
-      <TransitionGroup tag="ul" name="list">
-        <li v-if="!folder.root" key="cd.." class="item">
-          <button class="item__name" @click="goUpFolder">
-            cd ..
-          </button>
-        </li>
+    <TransitionGroup v-if="folder" :key="folder.path" tag="ul" name="list">
+      <li v-if="!folder.root" key="cd.." class="item">
+        <button class="item__name" @click="goUpFolder">
+          cd ..
+        </button>
+      </li>
 
-        <li v-for="item in mergedContents" :key="item.id.toString()">
-          <WorkspaceContentsItem :item="item" :parent="folder" />
-        </li>
-      </TransitionGroup>
-    </template>
+      <li v-for="item in mergedContents" :key="item.id.toString()">
+        <WorkspaceContentsItem :item="item" :parent="folder" />
+      </li>
+    </TransitionGroup>
 
-    <template v-else>
-      <WorkspaceContentsSkeleton />
-    </template>
+    <WorkspaceContentsSkeleton v-else />
   </Transition>
-
-  <button class="workspace__create-button" @click="folder && preCreateItem(folder)">
-    <Icon name="ic:outline-add" />
-  </button>
 </template>
-
-<style lang="scss">
-.workspace {
-  &__create-button {
-    --button-size-basis: 10vw;
-    --button-size-max: 4rem;
-    --button-size-min: 3.9rem;
-
-    position: absolute;
-    bottom: calc(var(--button-size-max) / 3);
-    left: calc(var(--button-size-max) / 3);
-    z-index: 1;
-
-    height: var(--button-size-basis);
-    width: var(--button-size-basis);
-
-    max-width: var(--button-size-max);
-    min-width: var(--button-size-min);
-    max-height: var(--button-size-max);
-    min-height: var(--button-size-min);
-
-    appearance: none;
-
-    border: none;
-    border-radius: 50%;
-    background: hsla(var(--text-color-hsl), .7);
-    box-shadow: 0 0 1rem hsla(var(--text-color-hsl), .1);
-
-    cursor: pointer;
-    transition: background-color .3s;
-
-    svg {
-      color: hsla(var(--surface-color-hsl), 0.75);
-
-      height: 50%;
-      width: auto;
-
-      transition: color .3s;
-    }
-
-    &:is(:hover, :focus-visible) {
-      background-color: hsla(var(--text-color-hsl), 1);
-
-      svg {
-        color: hsla(var(--surface-color-hsl), 1);
-      }
-    }
-
-    @media screen and (max-width: $breakpoint-tablet) {
-      right: calc(var(--button-size-max) / 3);
-      left: unset;
-    }
-  }
-}
-</style>
