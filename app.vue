017f06590dd06fd4944127b574aed959fe978401
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-29 23:05:11 +0200

    fix: correct font preloading without origin

--- app.vue
+++ app.vue
@@ -1,23 +1,17 @@
-<script setup>
+<script setup lang="ts">
 import MonaSansUrl from '~/assets/fonts/Mona-Sans/Mona-Sans.woff2';
 
-if (process.server) {
-  const localhost = 'localhost:3000';
-  const base = process.env.SITE_ORIGIN || localhost;
-  const protocol = base === localhost ? 'http://' : 'https://';
-
-  useHead({
-    link: [
-      {
-        rel: 'preload',
-        as: 'font',
-        type: 'font/woff2',
-        crossorigin: 'anonymous',
-        href: `${protocol}${base}${MonaSansUrl}`,
-      },
-    ],
-  });
-}
+useHead({
+  link: [
+    {
+      rel: 'preload',
+      as: 'font',
+      type: 'font/woff2',
+      crossorigin: 'anonymous',
+      href: MonaSansUrl.slice(1),
+    },
+  ],
+});
 </script>
 
 <template>
