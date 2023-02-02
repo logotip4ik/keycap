f21d10dc3560cc90fc57662660b43288ed9f6c10
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-02-02 19:34:00 +0200

    refactor: type h3 event context

--- types/server.d.ts
+++ types/server.d.ts
@@ -1,8 +1,16 @@
+import type { User } from '@prisma/client'
+
 declare global {
   // @link https://github.com/unjs/ofetch#%EF%B8%8F-handling-errors
   // ofetch bundled with nuxt will automatically throw error ok:false
   interface QuickResponse { 
     ok: boolean
+  }
+}
+
+export declare module 'H3' {
+  interface H3EventContext {
+    user: Pick<User, 'id' | 'username' | 'email'> | null
   }
 }
 
