1c1633557141b13cc6f6c8932fc89e2cfe4ab081
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-02-05 16:47:50 +0200

    feat: favicons and webmanifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,6 +52,12 @@ export default defineNuxtConfig({
         { name: 'apple-mobile-web-app-status-bar-style', content: 'default', media: '(prefers-color-scheme: light)' },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent', media: '(prefers-color-scheme: dark)' },
       ],
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest', crossorigin: 'use-credentials' },
+      ],
     },
   },
 
