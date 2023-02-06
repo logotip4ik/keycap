90098fb8e56041a858a531fc4b2fa890c75b7a93
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-02-06 11:23:14 +0200

    refactor: use relative paths for webmanifest and remove id property

--- server/routes/site.webmanifest.ts
+++ server/routes/site.webmanifest.ts
@@ -1,27 +1,12 @@
-import { isDevelopment } from 'std-env';
-
 import manifestSchema from '~/assets/constants/webmanifest.json';
 
 export default defineEventHandler((event) => {
   const { user } = event.context;
 
-  const protocol = isDevelopment ? 'http://' : 'https://';
-  const siteOrigin = process.env.SITE_ORIGIN || 'localhost:3000';
-  const siteUrl = protocol + siteOrigin;
+  const manifest: typeof manifestSchema = { ...manifestSchema };
 
-  const manifest: typeof manifestSchema = {
-    ...manifestSchema,
-    id: siteUrl,
-    scope: siteUrl,
-    start_url: siteUrl,
-  };
-
-  if (user) {
-    const userSuffix = `/@${user.username}`;
-
-    manifest.id += userSuffix;
-    manifest.start_url += userSuffix;
-  }
+  if (user)
+    manifest.start_url += `@${user.username}`;
 
   return manifest;
 });
