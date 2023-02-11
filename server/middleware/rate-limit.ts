f2595dec573799fe91be35ec9ac142e018dca576
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-02-11 15:10:04 +0200

    feat: i guess working rate limiting middleware

--- server/middleware/rate-limit.ts
+++ server/middleware/rate-limit.ts
@@ -1,58 +1,30 @@
-import { RateLimiter } from 'limiter';
-import cache from 'memory-cache';
+import RateLimiter from 'lambda-rate-limiter';
 
-import type { RateLimiterOpts } from 'limiter';
+const INTERVAL = 60000; // 1 minute
+const LIMIT = Math.floor((INTERVAL * 2) / 1000); // two per second
 
-// 1 hour in milliseconds
-const interval = 60 * 60 * 1000;
-const limiterOptions: RateLimiterOpts = {
-  interval,
-  tokensPerInterval: 150,
-};
+const { check: rateLimit } = RateLimiter({
+  interval: INTERVAL,
+  uniqueTokenPerInterval: LIMIT,
+});
 
+// NOTE: https://lihbr.com/posts/rate-limiting-without-overhead-netlify-or-vercel-functions
+// cool article that describes problem about rate limiting in serverless functions
 export default defineEventHandler(async (event) => {
-  // This will never work correctly
-  // limiter is stored for an hour in cache
-  // deletion callback is in setTimeout, and node
-  // will wait for that timeout before exiting
-  // serverless function, so what's the point
-  // of it ?
-  // https://github.com/jhurliman/node-rate-limiter/blob/main/src/TokenBucket.ts
-  // Another option is to use this approach, but still
-  // this will store limiter for a 10 seconds timeout,
-  // but user can define another interval for limiter
-  // package. Then what's the point of interval, from options
-  // the limiter will still be removed from cache after 10
-  // seconds
-  // Probably most valid correct rate limiter for
-  // serverless endpoints would be (redis | dragonfly)
-  // cache, that will be stored and deleted correctly
   if (event.path?.startsWith('/api')) {
-    const identifier = getHeader(event, 'x-forwarded-for');
+    const identifier = getHeader(event, 'x-real-ip') || getHeader(event, 'x-forwarded-for');
 
-    let limiter = cache.get(identifier) as RateLimiter | null;
+    const used = await rateLimit(LIMIT, identifier!).catch(() => LIMIT);
 
-    if (!limiter) {
-      limiter = new RateLimiter(limiterOptions);
+    appendHeader(event, 'X-RateLimit-Limit', LIMIT.toString());
+    appendHeader(event, 'X-RateLimit-Remaining', (LIMIT - used).toString());
 
-      // NOTE: migrate to unstorage when native ttl is supported ? https://github.com/unjs/unstorage/issues/13
-      cache.put(identifier, limiter, interval);
+    // Too many requests
+    if (used === null) {
+      return createError({
+        statusCode: 429,
+        statusMessage: 'Too many requests',
+      });
     }
-
-    let points = 1;
-
-    if (event.path.startsWith('/api/user'))
-      points += 2;
-
-    if (!event.context.user)
-      points *= 3;
-
-    const remainingPoints = await limiter.removeTokens(points);
-
-    appendHeader(event, 'X-RateLimit-Limit', interval.toString());
-    appendHeader(event, 'X-RateLimit-Remaining', remainingPoints.toString());
-
-    if (remainingPoints <= 0)
-      return createError({ statusCode: 429 });
   }
 });
