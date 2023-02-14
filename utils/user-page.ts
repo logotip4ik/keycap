7be0cee1f9b04ab703df1c4cee1f64510e760469
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-02-14 13:43:47 +0200

    chore: add fallback if items is null

--- utils/user-page.ts
+++ utils/user-page.ts
@@ -51,7 +51,7 @@ export async function defineOfflineStorage() {
     if (!fallbackToast)
       fallbackToast = createToast('Fallback mode enabled. Populating cache from offline storage.');
 
-    const items = await values<FolderOrNote>();
+    const items = await values<FolderOrNote>() || [];
 
     for (const item of items) {
       const isFolder = 'root' in item;
