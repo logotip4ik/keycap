e259cd46f6ec735493c1ea23fb65097b898df869
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-02-15 13:32:38 +0200

    chore: enable dns prefetch control header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,7 @@ const defaultHeaders = {
   // 'Cross-Origin-Opener-Policy': 'same-origin',
   // 'Cross-Origin-Resource-Policy': 'same-origin',
   // 'Origin-Agent-Cluster': '?1',
-  // 'X-DNS-Prefetch-Control': 'off',
+  'X-DNS-Prefetch-Control': 'off',
   'X-Download-Options': 'noopen',
   'X-Permitted-Cross-Domain-Policies': 'none',
   'X-Frame-Options': 'DENY',
