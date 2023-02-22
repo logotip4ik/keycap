1f2b96d867a6e49fb4c4843e2e9e159f0ee9b9c5
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-02-22 13:03:10 +0200

    feat: use cached event handler for manifest generation

--- server/routes/site.webmanifest.ts
+++ server/routes/site.webmanifest.ts
@@ -1,12 +1,22 @@
 import manifestSchema from '~/assets/constants/webmanifest.json';
+import { WEEK_IN_SECONDS } from '~/headers.config';
 
-export default defineEventHandler((event) => {
+const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+
+export default defineCachedEventHandler((event) => {
   const { user } = event.context;
 
-  const manifest: typeof manifestSchema = { ...manifestSchema };
+  const manifest = { ...manifestSchema } satisfies typeof manifestSchema;
 
   if (user)
     manifest.start_url += `@${user.username}`;
 
   return manifest;
+}, {
+  name: 'manifest',
+  swr: true,
+  maxAge: ONE_DAY_IN_SECONDS,
+  staleMaxAge: WEEK_IN_SECONDS,
+  getKey: (event) => event.context.user ? `manifest-${event.context.user.username}` : 'manifest',
+  shouldInvalidateCache: (event) => typeof getQuery(event).invalidate !== 'undefined',
 });
