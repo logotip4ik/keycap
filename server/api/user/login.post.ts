d92a2324bfbd46ac75b9083e0c8827f013001458
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-03-04 19:21:41 +0200

    fix: correct validation for email

--- server/api/user/login.post.ts
+++ server/api/user/login.post.ts
@@ -5,7 +5,7 @@ import type { User } from '@prisma/client';
 import getPrisma from '~/prisma';
 
 const loginSchema = v.object({
-  email: v.string().minLength(4).required(),
+  email: v.string().format('email').required(),
   password: v.string().minLength(8).required(),
 });
 
