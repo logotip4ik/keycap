3cc4817217afa653b366fb03eabb7ff4d3b68113
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-03-09 23:28:30 +0200

    refactor: better support for small screens

--- components/workspace/Item/Details/ItemDetails.vue
+++ components/workspace/Item/Details/ItemDetails.vue
@@ -64,6 +64,10 @@ useClickOutside(itemDetailsEl, () => currentItemForDetails.value = null);
 
     background-color: hsla(var(--surface-color-hsl), 0.9);
 
+    @media screen and (max-width: $breakpoint-tablet) {
+      align-items: stretch;
+    }
+
     @supports (backdrop-filter: blur(1px)) {
       background-color: hsla(var(--surface-color-hsl), 0.25);
       backdrop-filter: blur(0.5rem);
