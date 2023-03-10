0f231043b3438213aebeabcf5989613d95d5928a
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-03-10 20:29:04 +0200

    refactor: remove font preloaing from client bundle

--- app.vue
+++ app.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-useHead({
-  link: [
-    {
-      rel: 'preload',
-      as: 'font',
-      type: 'font/woff2',
-      crossorigin: 'anonymous',
-      // @ts-expect-error no types for font
-      href: import('~/assets/fonts/Mona-Sans/Mona-Sans.woff2?url').then((url) => url.default),
-    },
-  ],
-}, { mode: 'server' });
+if (process.server) {
+  useHead({
+    link: [
+      {
+        rel: 'preload',
+        as: 'font',
+        type: 'font/woff2',
+        crossorigin: 'anonymous',
+        // @ts-expect-error no types for font
+        href: import('~/assets/fonts/Mona-Sans/Mona-Sans.woff2?url').then((url) => url.default),
+      },
+    ],
+  }, { mode: 'server' });
+}
 </script>
 
 <template>
