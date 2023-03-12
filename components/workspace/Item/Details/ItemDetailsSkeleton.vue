92a5f88438da07ef1c94c240f5761ac4ed28ae37
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-03-12 14:53:24 +0200

    refactor: better skeleton for item details popup

--- components/workspace/Item/Details/ItemDetailsSkeleton.vue
+++ components/workspace/Item/Details/ItemDetailsSkeleton.vue
@@ -2,10 +2,53 @@
   <div class="skeleton">
     <h3 class="skeleton__heading skeleton-loading-bg" />
 
-    <p class="skeleton__text skeleton-loading-bg" />
-    <p class="skeleton__text skeleton-loading-bg" />
-    <p class="skeleton__text skeleton-loading-bg" />
-    <p class="skeleton__text skeleton-loading-bg" />
-    <p class="skeleton__text skeleton-loading-bg" />
+    <div class="skeleton__row">
+      <p class="skeleton__row__text skeleton-loading-bg" />
+      <p class="skeleton__row__date skeleton-loading-bg" />
+    </div>
+    <div class="skeleton__row">
+      <p class="skeleton__row__text skeleton-loading-bg" />
+      <p class="skeleton__row__date skeleton-loading-bg" />
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+@use "sass:math";
+
+.skeleton__row {
+  $numberOfItems: 2;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & + & {
+    margin-top: 0.75rem;
+  }
+
+  &__text,
+  &__date {
+    height: 1.5rem;
+
+    margin: 0;
+
+    border-radius: 0.15rem;
+  }
+
+  @for $i from 1 to $numberOfItems + 1 {
+    &:nth-of-type(#{$i}) {
+      .skeleton__row__text,
+      .skeleton__row__date {
+        width: (random(15) + 17)+#{'%'};
+
+        opacity: math.div(1, $i + 1);
+      }
+
+      .skeleton__row__text {
+        width: (random(15) + 17)+#{'%'};
+      }
+    }
+  }
+}
+</style>
