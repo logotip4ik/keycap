33c0040c1bbfb6753852cd8aa6b3ed4b00a26f06
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-04-10 15:23:06 +0300

    feat: route for fetching shared note

---
+++ server/api/share/[link].get.ts
@@ -0,0 +1,20 @@
+import getPrisma from '~/prisma';
+
+export default defineEventHandler(async (event) => {
+  const link = getRouterParam(event, 'link');
+
+  if (!isShareLinkValid(link))
+    return createError({ statusCode: 404 });
+
+  const prisma = getPrisma();
+
+  const note = await prisma.note.findFirst({
+    select: { name: true, content: true, updatedAt: true, createdAt: true },
+    where: { share: { some: { link } } },
+  }).catch(() => null);
+
+  if (!note)
+    return createError({ statusCode: 404 });
+
+  return note;
+});
