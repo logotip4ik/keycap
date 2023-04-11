c486bef3ed8b7c8e4b37f268c7c5753708092591
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-04-11 22:30:45 +0300

    refactor: lower animation duration

--- pages/index.vue
+++ pages/index.vue
@@ -139,7 +139,7 @@ const shortCommitSha = buildInfo.commit;
       filter: blur(calc(var(--size) / var(--blur-divider)));
 
       @media (prefers-reduced-motion: no-preference) {
-        animation: blob-anim 25s infinite linear alternate;
+        animation: blob-anim 15s infinite linear alternate;
       }
 
       @media screen and (max-width: $breakpoint-tablet) {
