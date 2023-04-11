4cffb8ea8716ea202aa95fa7649e372c6c09c4cf
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-04-11 16:31:44 +0300

    feat: add note renderer

---
+++ components/NoteRendered.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import '~/assets/styles/note-editor.scss';
+
+import { EditorContent, useEditor } from '@tiptap/vue-3';
+
+import Document from '@tiptap/extension-document';
+import Paragraph from '@tiptap/extension-paragraph';
+import Text from '@tiptap/extension-text';
+import Bold from '@tiptap/extension-bold';
+import Italic from '@tiptap/extension-italic';
+import Strike from '@tiptap/extension-strike';
+import Blockquote from '@tiptap/extension-blockquote';
+import BulletList from '@tiptap/extension-bullet-list';
+import HardBreak from '@tiptap/extension-hard-break';
+import Heading from '@tiptap/extension-heading';
+import HorizontalRule from '@tiptap/extension-horizontal-rule';
+import ListItem from '@tiptap/extension-list-item';
+import OrderedList from '@tiptap/extension-ordered-list';
+import Code from '@tiptap/extension-code';
+import Link from '@tiptap/extension-link';
+import TaskList from '@tiptap/extension-task-list';
+import TaskItem from '@tiptap/extension-task-item';
+import CodeBlock from '@tiptap/extension-code-block';
+
+interface Props { content: string }
+const props = defineProps<Props>();
+
+const editor = useEditor({
+  content: props.content,
+  editable: false,
+  extensions: [
+    Document,
+    Text,
+    Paragraph,
+    Blockquote,
+    BulletList,
+    HardBreak,
+    Heading.configure({ levels: [1, 2, 3] }),
+    HorizontalRule,
+    ListItem,
+    OrderedList,
+    Bold,
+    Italic,
+    Strike,
+    Link,
+    TaskList,
+    TaskItem,
+    Code.configure({
+      HTMLAttributes: {
+        autocomplete: 'off',
+        autocorrect: 'off',
+        autocapitalize: 'none',
+        spellcheck: 'false',
+      },
+    }),
+    // TODO: https://tiptap.dev/api/nodes/code-block-lowlight
+    CodeBlock.extend({
+      addAttributes() {
+        return {
+          ...this.parent?.(),
+          autocomplete: { default: 'off' },
+          autocorrect: { default: 'off' },
+          autocapitalize: { default: 'none' },
+          spellcheck: { default: 'false' },
+        };
+      },
+    }),
+  ],
+});
+
+watch(() => props.content, (content) => {
+  const editorContent = editor.value?.getHTML();
+
+  if (editorContent !== content) editor.value?.commands.setContent(content);
+});
+</script>
+
+<template>
+  <EditorContent class="note-editor" :editor="editor" />
+</template>
