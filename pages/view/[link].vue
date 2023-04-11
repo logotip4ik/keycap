7e626f2e80881c0502a2ff131b8e792d8a5441b7
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-04-11 16:32:53 +0300

    feat: render note contents

--- pages/view/[link].vue
+++ pages/view/[link].vue
@@ -7,13 +7,16 @@ const { data: note, error } = await useAsyncData<Pick<Note, 'name' | 'content' |
   () => $fetch(`/api/share/${route.params.link}`),
 );
 
-// TODO: export note editor styles to separate file and include here
-// TODO: create client only component similar to NoteEditor but smaller in size and readonly state
-
 useHead({
   title: note.value?.name,
   titleTemplate: '%s - Keycap',
 });
+
+function formatDate(date: string | Date) {
+  date = new Date(date);
+
+  return Intl.DateTimeFormat(undefined, { dateStyle: 'short' }).format(date);
+}
 
 watch(error, (error) => {
   if (error) {
@@ -26,18 +29,61 @@ watch(error, (error) => {
 </script>
 
 <template>
-  <div v-if="note">
-    <header>
-      <!-- TODO: style as h1 -->
-      <p>
+  <div v-if="note" class="note-view">
+    <header class="note-view__header">
+      <p class="note-view__header__name">
         {{ note.name }}
       </p>
-      <p>{{ note.updatedAt }}</p>
+      <p class="note-view__header__updated-at">
+        Last update at: {{ formatDate(note.updatedAt) }}
+      </p>
     </header>
 
-    <!-- TODO: render content with Client only component, similar to note editor
-    {{ note.content }} -->
-    <!-- TODO: remove v-html -->
-    <main v-html="note.content" />
+    <!-- TODO: render content with Client only component, similar to note editor -->
+    <main class="note-view__main">
+      <NoteRendered class="note-view__main__note-renderer" :content="note.content!" />
+    </main>
   </div>
 </template>
+
+<style lang="scss">
+.note-view {
+  width: 95%;
+  height: 100%;
+
+  max-width: 1200px;
+
+  padding: 25vh 0 0;
+  margin: 0 auto;
+
+  &__header {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+
+    margin-bottom: 10vh;
+
+    &__name {
+      font-size: clamp(3.5rem, 4vw + 1rem, 5rem);
+      font-weight: 500;
+
+      margin: 0 0 0.5rem;
+    }
+
+    &__updated-at {
+      opacity: 0.75;
+      margin: 0;
+    }
+  }
+
+  &__main {
+    &__note-renderer {
+      .ProseMirror {
+        padding-top: 0;
+        padding-inline: 0;
+      }
+    }
+  }
+}
+</style>
