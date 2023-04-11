9f0c61430c1f9b9ffe875a15ce1ee3ddeaeaaadb
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-04-11 22:20:58 +0300

    refactor: add minimal heading

--- pages/view/[link].vue
+++ pages/view/[link].vue
@@ -32,6 +32,19 @@ watch(error, (error) => {
 
 <template>
   <div v-if="note" class="note-view">
+    <nav class="note-view__nav">
+      <NuxtLink class="note-view__nav__title" to="/">
+        <img
+          src="/favicon-32x32.png"
+          alt="purple keycap"
+          class="note-view__nav__title__img"
+          fetchpriority="high"
+          decoding="async"
+        >
+        Keycap
+      </NuxtLink>
+    </nav>
+
     <header class="note-view__header">
       <p class="note-view__header__name">
         {{ note.name }}
@@ -50,6 +63,9 @@ watch(error, (error) => {
 
 <style lang="scss">
 .note-view {
+  position: relative;
+  isolation: isolate;
+
   width: 95%;
   height: 100%;
 
@@ -57,6 +73,53 @@ watch(error, (error) => {
 
   padding: 25vh 0 0;
   margin: 0 auto;
+
+  &__nav {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: calc(3vh + 1vw);
+
+    width: 100%;
+
+    &__title {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+
+      font-size: 1.25rem;
+      font-weight: 500;
+      font-stretch: 110%;
+      text-decoration: none;
+      color: var(--text-color);
+
+      margin: 0;
+
+      opacity: 0.8;
+
+      transition: opacity .3s, filter .3s;
+
+      &__img {
+        display: block;
+
+        width: 1.75rem;
+        height: auto;
+
+        margin-right: 0.25rem;
+      }
+
+      &:is(:hover, :focus-visible) {
+        transition-duration: .1s;
+
+        opacity: 1;
+        filter: drop-shadow(0px 0px 0.5rem hsla(var(--text-color-hsl), 0.125));
+      }
+    }
+  }
 
   &__header {
     display: flex;
