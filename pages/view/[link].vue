2c80b970a427b28b09d11acd3aa855132ab589db
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-04-14 15:59:00 +0300

    feat: empty note indicator

--- pages/view/[link].vue
+++ pages/view/[link].vue
@@ -12,6 +12,8 @@ useHead({
   titleTemplate: '%s - Keycap',
 });
 
+const isNoteContentsEmpty = note.value?.content && note.value.content.replace(/<[^>]+>/g, '').trim() === '';
+
 function formatDate(date: string | Date) {
   date = new Date(date);
 
@@ -22,10 +24,10 @@ watch(error, (error) => {
   if (error) {
     showError({
       statusCode: 404,
-      statusMessage: `nothing found with this link: ${route.params.link}`,
+      statusMessage: `nothing found with link:${route.params.link}`,
     });
   }
-});
+}, { immediate: true });
 </script>
 
 <template>
@@ -44,9 +46,15 @@ watch(error, (error) => {
     </nav>
 
     <header class="note-view__header">
+      <small v-if="isNoteContentsEmpty" class="note-view__header__alert">
+        <Icon name="ic:outline-info" class="note-view__header__alert__icon" />
+        Note is empty
+      </small>
+
       <p class="note-view__header__name">
         {{ note.name }}
       </p>
+
       <p class="note-view__header__updated-at">
         Last update at: {{ formatDate(note.updatedAt) }}
       </p>
@@ -127,11 +135,38 @@ watch(error, (error) => {
 
     margin-bottom: 10vh;
 
+    &__alert {
+      display: inline-block;
+
+      color: hsla(var(--text-color-hsl), 0.8);
+
+      margin: 0;
+      padding: 0.5rem 0.75rem;
+
+      border-radius: 0.25rem;
+      border: 1px solid hsla(var(--selection-bg-color-hsl), 0.5);
+      background-color: hsla(var(--selection-bg-color-hsl), 0.125);
+
+      @media (prefers-color-scheme: dark) {
+        background-color: hsla(var(--selection-bg-color-hsl), 0.25);
+      }
+
+      &__icon {
+        margin-right: 0.25rem;
+
+        vertical-align: baseline  !important;
+
+        transform: translateY(1.5px);
+      }
+    }
+
     &__name {
-      font-size: clamp(3.5rem, 4vw + 1rem, 5rem);
+      font-size: clamp(3.5rem, 4vw + 1.125rem, 5rem);
+      line-height: 1.25;
       font-weight: 500;
 
-      margin: 0 0 0.5rem;
+      margin: 0;
+      margin-bottom: 0.5rem;
     }
 
     &__updated-at {
