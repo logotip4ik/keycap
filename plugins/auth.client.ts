560a27ebc38dd37646ae49959ee449cc1f484350
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-04-19 17:22:51 +0300

    fix: type error

--- plugins/auth.client.ts
+++ plugins/auth.client.ts
@@ -1,3 +1,5 @@
+import { toBigInt } from '~/server/utils';
+
 export default defineNuxtPlugin(() => {
   const route = useRoute();
   const user = useUser();
@@ -5,7 +7,7 @@ export default defineNuxtPlugin(() => {
   if (user.value) return;
 
   $fetch('/api/user/me')
-    .then((fetchedUser) => user.value = fetchedUser)
+    .then((fetchedUser) => user.value = { ...fetchedUser, id: toBigInt(fetchedUser.id) })
     // https://github.com/unjs/ofetch#%EF%B8%8F-handling-errors
     .catch(async (error) => {
       if (error.statusCode === 401 && route.path.includes('/@'))
