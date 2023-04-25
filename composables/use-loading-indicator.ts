f83d1b66b68242dd90fc9f41ff8302de97b3df9d
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-04-25 22:26:31 +0300

    refactor: more stable loading indicator toggler

--- composables/use-loading-indicator.ts
+++ composables/use-loading-indicator.ts
@@ -1,24 +1,24 @@
 import type { RuntimeNuxtHooks } from 'nuxt/app';
 import type { HookKeys } from 'hookable';
 
-let isShowingLoadingIndicator = false;
+const isShowingLoading = { v: false };
 
 // https://github.com/nuxt/nuxt/issues/14221#issuecomment-1397723845
 export default () => {
   return {
-    showLoading: createToggle('page:start', true, () => isShowingLoadingIndicator),
-    hideLoading: createToggle('page:finish', false, () => !isShowingLoadingIndicator),
+    showLoading: createToggle('page:start', true, () => isShowingLoading.v),
+    hideLoading: createToggle('page:finish', false, () => !isShowingLoading.v),
   };
 };
 
 function createToggle(hookName: HookKeys<RuntimeNuxtHooks>, setValue: any, shouldIgnore: () => any) {
-  const nuxtApp = useNuxtApp();
-
   return () => {
     if (shouldIgnore())
       return;
 
+    const nuxtApp = useNuxtApp();
+
     nuxtApp.callHook(hookName);
-    isShowingLoadingIndicator = setValue;
+    isShowingLoading.v = setValue;
   };
 }
