e8d2a5d85394f4a5d1767f6027739a2f1d043441
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-04-29 12:54:15 +0300

    refactor: better bigint to json and use array.at polyfill

--- server/plugins/bigint.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-extend-native
-BigInt.prototype.toJSON = function () {
-  return `${this.toString()}n`;
-};
-
-export default defineNitroPlugin(() => null);
---
+++ server/plugins/chore.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-invalid-this */
+
+import '~/polyfills/array-at';
+
+function toJSON() {
+  // @ts-expect-error this should work ಠ_ಠ
+  return `${this.toString()}n`;
+}
+
+// eslint-disable-next-line no-extend-native
+Object.defineProperty(
+  BigInt.prototype,
+  'at',
+  {
+    value: toJSON,
+    writable: true,
+    enumerable: false,
+    configurable: true,
+  },
+);
+
+export default defineNitroPlugin(() => undefined);
