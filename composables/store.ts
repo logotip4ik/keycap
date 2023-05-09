b847dd1e0ce5e4e7b42dfca430239aad59a2eb94
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-05-09 23:12:08 +0300

    fix: do not break types

--- composables/store.ts
+++ composables/store.ts
@@ -15,7 +15,7 @@ export const useFoldersCache = () => foldersCache;
 const fuzzyWorker = shallowRef<null | IFuzzyWorker>(null);
 export const useFuzzyWorker = () => fuzzyWorker;
 
-const offlineStorage = shallowRef<false | OfflineStorage>(process.client && getOfflineStorage());
+const offlineStorage = shallowRef<null | OfflineStorage>(process.server ? null : getOfflineStorage());
 export const useOfflineStorage = () => offlineStorage;
 
 export function deleteNoteFromFolder(noteToDelete: NoteMinimal, parent: FolderWithContents) {
