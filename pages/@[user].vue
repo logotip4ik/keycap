a137baf0e139e73453cc530f5920a61e9f57c67c
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-06-10 22:50:15 +0300

    refactor: wrap each function into idle callback

--- pages/@[user].vue
+++ pages/@[user].vue
@@ -79,11 +79,8 @@ onMounted(() => {
   if (typeof route.query.search !== 'undefined')
     isShowingSearch.value = true;
 
-  // request idle callback is polyfilled by nuxt
-  window.requestIdleCallback(() =>
-    [preloadDashboardComponents, defineFuzzyWorker]
-      .map((cb) => cb()),
-  );
+  [preloadDashboardComponents, defineFuzzyWorker, updateServiceWorker]
+    .map((cb) => window.requestIdleCallback(cb));
 
   if (process.dev)
     // @ts-expect-error this should not be defined
