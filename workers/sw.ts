d5f1ef2e1c6efaf9b94d0642a0772da4faea652f
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-06-11 19:36:40 +0300

    refactor: skip waiting only after message

--- workers/sw.ts
+++ workers/sw.ts
@@ -113,6 +113,10 @@ setCatchHandler(({ event }): Promise<Response> => {
   }
 });
 
-self.skipWaiting();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING')
+    self.skipWaiting();
+});
+
 clientsClaim();
 cleanupOutdatedCaches();
