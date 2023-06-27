64e62c8edd81d3d9a8c8f5bc8ba6aa34c3bb299d
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-06-27 23:09:08 +0300

    refactor: transform share deletion into transaction

--- server/api/share/note/[...path].delete.ts
+++ server/api/share/note/[...path].delete.ts
@@ -12,38 +12,34 @@ export default defineEventHandler(async (event) => {
   const notePath = generateNotePath(user.username, path);
 
   const prisma = getPrisma();
-  const storage = useStorage();
 
   timer.start('db');
 
-  const shareToDelete = await prisma.share.findFirst({
-    where: {
-      AND: {
+  // NOTE: there is no point from removing cached page because website is hosted on vercel
+
+  const error = await prisma.$transaction(async (tx) => {
+    const shareToDelete = await tx.share.findFirst({
+      where: {
         owner: { id: user.id },
         note: { path: notePath },
       },
-    },
-    select: { id: true, link: true },
-  }).catch(() => null);
+      select: { id: true, link: true },
+    }).catch(() => null);
 
-  if (!shareToDelete)
-    return createError({ statusCode: 400 });
+    if (!shareToDelete)
+      return createError({ statusCode: 400 });
 
-  // how to find correct key https://github.com/unjs/nitro/blob/30675d4353f366395cdc0d53e9512aaa3f7c4bf7/src/runtime/cache.ts#L184
-  const friendlyShareLink = shareToDelete.link.replace(/-/g, '');
+    const deletedShare = await tx.share.delete({
+      where: { id: shareToDelete.id },
+      select: { id: true },
+    }).catch(() => null);
 
-  const [cacheKeys, share] = await Promise.all([
-    storage.getKeys('cache/nitro'),
-    prisma.share.delete({ where: { id: shareToDelete.id }, select: { id: true } }),
-  ]).catch(() => [null, null]);
+    if (!deletedShare)
+      return createError({ statusCode: 400 });
+  });
 
-  if (!cacheKeys || !share)
-    return createError({ statusCode: 400 });
-
-  const keyToDelete = cacheKeys.find((key) => key.includes(friendlyShareLink));
-
-  if (keyToDelete)
-    await storage.removeItem(keyToDelete);
+  if (error)
+    return error;
 
   timer.end();
 
