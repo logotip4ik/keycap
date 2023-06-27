8ba9faf8db9997ca43cb41ed95acba0f7c5f13d7
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-06-27 23:44:51 +0300

    feat: `createLogger` function

---
+++ server/utils/logger.ts
@@ -0,0 +1,47 @@
+import { isDevelopment, isProduction } from 'std-env';
+
+import * as winston from 'winston';
+import { WinstonTransport as AxiomTransport } from '@axiomhq/axiom-node';
+
+let loggerInstance: winston.Logger;
+
+export function createLogger() {
+  if (loggerInstance)
+    return loggerInstance;
+
+  const { combine, errors, json } = winston.format;
+
+  loggerInstance = winston.createLogger({
+    level: 'info',
+    format: combine(errors({ stack: true }), json()),
+    defaultMeta: { service: 'nitro' },
+  });
+
+  if (isProduction) {
+    const { axiomApiToken, axiomDataset, axiomOrgId } = useRuntimeConfig();
+
+    const axiom = new AxiomTransport({
+      orgId: axiomOrgId,
+      token: axiomApiToken,
+      dataset: axiomDataset,
+    });
+
+    // @ts-expect-error eeeeeeeeeeee, idk, hope it will work
+    loggerInstance.add(axiom);
+    // @ts-expect-error eeeeeeeeeeee, idk, hope it will work
+    loggerInstance.exceptions.handle(axiom);
+    // @ts-expect-error eeeeeeeeeeee, idk, hope it will work
+    loggerInstance.rejections.handle(axiom);
+  }
+
+  // Add the console logger if we're not in production
+  if (isDevelopment) {
+    loggerInstance.add(
+      new winston.transports.Console({
+        format: winston.format.simple(),
+      }),
+    );
+  }
+
+  return loggerInstance;
+}
