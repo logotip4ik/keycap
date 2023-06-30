6b9af118149643d0f324acdd42cf1076135f8db3
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-06-30 18:22:50 +0300

    feat: inline and bubble menu positioners

---
+++ components/workspace/Note/Editor/Formatter/BubbleMenu.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/shift-away.css';
+
+import { BubbleMenu } from '@tiptap/vue-3';
+
+import type { Editor } from '@tiptap/core';
+import type { Props as TippyProps } from 'tippy.js';
+
+interface Props { editor: Editor }
+defineProps<Props>();
+
+const isPhoneScreen = inject(IsSmallScreenKey)!;
+
+const tippyOptions: Partial<TippyProps> = {
+  // this element will never be displayed on server, so this should work
+  appendTo: document.body,
+  zIndex: 9,
+  duration: [50, 150],
+  theme: 'adaptive',
+  animation: 'shift-away',
+  arrow: false,
+  placement: isPhoneScreen.value ? 'bottom' : 'top',
+  offset: isPhoneScreen.value ? [0, 20] : [0, 10], // 10 is default
+};
+</script>
+
+<template>
+  <BubbleMenu
+    :editor="editor"
+    :tippy-options="tippyOptions"
+    class="formatter"
+  >
+    <slot />
+  </BubbleMenu>
+</template>
+
+<style lang="scss">
+.tippy-box[data-theme~='adaptive'] {
+  background-color: hsla(var(--surface-color-hsl), 0.95);
+
+  box-shadow:
+    1px 1.7px 5.3px rgba(0, 0, 0, 0.032),
+    3.4px 5.6px 17.9px rgba(0, 0, 0, 0.048),
+    15px 25px 80px rgba(0, 0, 0, 0.08)
+  ;
+
+  @supports (backdrop-filter: blur(1px)) {
+    background-color: hsla(var(--surface-color-hsl), 0.65);
+
+    backdrop-filter: blur(0.4rem);
+  }
+}
+
+.tippy-content {
+  padding: 0.325rem;
+}
+</style>
---
+++ components/workspace/Note/Editor/Formatter/InlineMenu.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import parseDuration from 'parse-duration';
+
+import type { Editor } from '@tiptap/core';
+
+interface Props { editor: Editor }
+defineProps<Props>();
+
+const INITIAL_DELAY_TO_APPEAR = parseDuration('0.35s');
+
+const shouldShow = ref(false);
+const inlineMenu = ref<HTMLElement | null>(null);
+
+function isOverflown({ clientWidth, scrollWidth }: HTMLElement) {
+  const diff = scrollWidth - clientWidth;
+  return diff > 2;
+}
+
+async function addScrollIfNeeded() {
+  if (inlineMenu.value && isOverflown(inlineMenu.value?.children[0] as HTMLElement))
+    inlineMenu.value.classList.add('overflown');
+}
+
+onMounted(() => {
+  setTimeout(() => {
+    shouldShow.value = true;
+  }, INITIAL_DELAY_TO_APPEAR);
+});
+</script>
+
+<template>
+  <Transition name="fade" @enter="addScrollIfNeeded">
+    <div v-if="shouldShow" ref="inlineMenu" class="inline-menu formatter">
+      <slot />
+    </div>
+  </Transition>
+</template>
+
+<style lang="scss">
+.inline-menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 10;
+
+  padding: 0.325rem 1.125rem;
+
+  width: 100%;
+
+  background-color: hsla(var(--surface-color-hsl), 0.95);
+  box-shadow: 0 0 1rem hsla(var(--text-color-hsl), 0.125);
+
+  &.overflown {
+    justify-content: flex-start;
+
+    overflow: auto;
+    scroll-snap-type: x proximity;
+    scroll-padding-inline: 1.125rem;
+
+    scrollbar-width: none;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  @supports (backdrop-filter: blur(1px)) {
+    background-color: hsla(var(--surface-color-hsl), 0.65);
+
+    backdrop-filter: blur(0.4rem);
+  }
+}
+</style>
