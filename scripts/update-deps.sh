3f9cfed7b5d0f305b65b8006c7f4e90f73d230ef
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-07-05 18:57:05 +0300

    refactor: use customized update script

---
+++ scripts/update-deps.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# fail as soon as any command errors
+set -e
+
+token=$1
+update_command=$2
+on_changes_command=$3
+repo=$GITHUB_REPOSITORY #owner and repository: ie: user/repo
+username=$GITHUB_ACTOR
+branch_name=$GITHUB_REF_NAME
+update_message="chore: update deps"
+
+email="noreply@github.com"
+
+if [ -z "$token" ]; then
+    echo "token is not defined"
+    exit 1
+fi
+
+if [ -z "$update_command" ]; then
+    echo "update-command cannot be empty"
+    exit 1
+fi
+
+# fetch first to be able to detect if branch already exists 
+git fetch
+
+echo "Running update command $update_command"
+eval $update_command
+
+if [ -n "git diff" ]
+then
+    echo "Updates detected"
+
+    # configure git authorship
+    git config --global user.email $email
+    git config --global user.name $username
+
+    # format: https://[username]:[token]@github.com/[organization]/[repo].git
+    git remote add authenticated "https://$username:$token@github.com/$repo.git"
+
+    # execute command to run when changes are deteced, if provided
+    on_changes_command_value=${on_changes_command%?}
+    echo $on_changes_command_value
+    if [ -n "$on_changes_command_value" ]; then
+        echo "Run post-update command"
+        eval $on_changes_command_value
+    fi
+
+    # explicitly add all files including untracked
+    git add -A
+
+    # commit the changes to updated files
+    git commit -a -m $update_message --signoff
+    
+    # push the changes
+    git push authenticated
+else
+    echo "No dependencies updates were detected"
+    exit 0
+fi
