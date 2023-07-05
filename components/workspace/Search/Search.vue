95ab0846ff6e2886cd2b3aa22ccd2719d82364f8
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-07-05 22:01:08 +0300

    refactor: use inset where possible

--- components/workspace/Search/Search.vue
+++ components/workspace/Search/Search.vue
@@ -215,10 +215,7 @@ useTinykeys({ Escape: handleCancel });
 
   &-wrapper {
     position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+    inset:0;
     z-index: 10;
 
     padding-top: 33vh;
--- pages/register.vue
+++ pages/register.vue
@@ -262,10 +262,7 @@ watch(user, async (value) => value && await navigateTo(`/@${value.username}`));
         }
 
         &::after {
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
+          inset: 0;
           z-index: 1;
 
           border-radius: inherit;
