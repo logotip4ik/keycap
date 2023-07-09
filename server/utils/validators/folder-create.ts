fb28e6ff3530f6de11e68741c0f0111fe56206cf
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-07-09 16:48:49 +0300

    feat: folder create validation schema

---
+++ server/utils/validators/folder-create.ts
@@ -0,0 +1,9 @@
+import { compile, v } from 'suretype';
+
+export const folderCreateSchema = v.object({
+  name: v.string().minLength(2).required(),
+  path: v.string().minLength(7).required(), // minLength 7 because - min username - 3 chars and min folder name - 2 + 2 slashes
+  parentId: v.string().matches(stringifiedBigIntRE).minLength(19).required(),
+});
+
+export const useFolderCreateSchema = compile(folderCreateSchema);
