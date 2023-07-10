ce03885624fc04bd679c34d654997fd0c7c9c615
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-07-10 20:37:47 +0300

    feat: github oauth endpoint

---
+++ server/api/oauth/github/index.get.ts
@@ -0,0 +1,83 @@
+import { isProduction } from 'std-env';
+import { withHttps, withQuery } from 'ufo';
+
+interface GitHubAuthRes {
+  access_token: string
+  scope: string
+  token_type: string
+}
+
+interface GitHubUserRes {
+  login: string
+  name: string
+  email: string | null
+}
+
+interface GitHubUserEmailRes {
+  email: string
+  primary: boolean
+  verified: boolean
+  visibility: string
+}
+
+export default defineEventHandler(async (event) => {
+  const { github } = useRuntimeConfig();
+  const code = getQuery(event).code;
+
+  if (!code) {
+    let redirectUrl = getRequestURL(event).href;
+
+    if (isProduction)
+      redirectUrl = withHttps(redirectUrl);
+
+    return sendRedirect(event,
+      withQuery('https://github.com/login/oauth/authorize', {
+        client_id: github.clientId,
+        redirect_uri: redirectUrl,
+        scope: 'user:email',
+      }),
+    );
+  }
+
+  const auth = await $fetch<GitHubAuthRes>('https://github.com/login/oauth/access_token', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+    },
+    query: {
+      code,
+      client_id: github.clientId,
+      client_secret: github.clientSecret,
+    },
+  }).catch(() => null);
+
+  if (!auth)
+    return sendRedirect(event, '/');
+
+  const headers = {
+    'Authorization': `${auth.token_type} ${auth.access_token}`,
+    'User-Agent': `Keycap${isProduction ? '' : ' Dev'}`,
+  };
+
+  const user = await $fetch<GitHubUserRes>('https://api.github.com/user', { headers })
+    .catch(() => null);
+
+  if (!user)
+    return sendRedirect(event, '/');
+
+  if (!user.email) {
+    const emails = await $fetch<GitHubUserEmailRes[]>('https://api.github.com/user/emails', { headers }).catch(() => null);
+
+    if (!emails)
+      return sendRedirect(event, '/');
+
+    const primaryEmail = emails.find((email) => email.primary);
+
+    if (!primaryEmail)
+      return sendRedirect(event, '/');
+
+    user.email = primaryEmail.email;
+  }
+
+  return user;
+});
