62d27edf4d0f38ddcad43ff6a33e7bc049030480
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-07-11 15:41:02 +0300

    feat: provider specific redirects and user gathering functions

---
+++ server/utils/oauth/github.ts
@@ -0,0 +1,84 @@
+import { isProduction } from 'std-env';
+
+import type { H3Event } from 'h3';
+import { withQuery } from 'ufo';
+
+export interface GitHubAuthRes {
+  access_token: string
+  scope: string
+  token_type: string
+}
+
+export interface GitHubUserRes {
+  id: number
+  login: string
+  name: string
+  email: string
+}
+
+export interface GitHubUserEmailRes {
+  email: string
+  primary: boolean
+  verified: boolean
+  visibility: string
+}
+
+export function sendGitHubOAuthRedirect(event: H3Event) {
+  const { github, public: config } = useRuntimeConfig();
+
+  const protocol = isProduction ? 'https://' : 'http://';
+
+  return sendRedirect(event,
+    withQuery('https://github.com/login/oauth/authorize', {
+      client_id: github.clientId,
+      redirect_uri: `${protocol}${config.siteOrigin}/api/oauth/github`,
+      scope: 'user:email',
+    }),
+  );
+}
+
+export async function getGitHubUserWithEvent(event: H3Event) {
+  const { github } = useRuntimeConfig();
+  const code = getQuery(event).code;
+
+  if (!code)
+    throw new Error('no code was found');
+
+  const auth = await $fetch<GitHubAuthRes>('https://github.com/login/oauth/access_token', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+    },
+    query: {
+      code,
+      client_id: github.clientId,
+      client_secret: github.clientSecret,
+    },
+  });
+
+  const apiAuthHeaders = {
+    'Authorization': `${auth.token_type} ${auth.access_token}`,
+    'User-Agent': `Keycap${isProduction ? '' : ' Dev'}`,
+  };
+
+  const user = await $fetch<GitHubUserRes>(
+    'https://api.github.com/user',
+    { headers: apiAuthHeaders },
+  );
+
+  if (!user.email) {
+    const emails = await $fetch<GitHubUserEmailRes[]>(
+      'https://api.github.com/user/emails',
+      { headers: apiAuthHeaders },
+    );
+
+    const primaryEmail = emails.find((email) => email.primary) || emails[0];
+
+    if (!primaryEmail)
+      throw new Error('was not able to gather user email');
+
+    user.email = primaryEmail.email;
+  }
+
+  return user;
+}
---
+++ server/utils/oauth/google.ts
@@ -0,0 +1,71 @@
+import { isProduction } from 'std-env';
+import { withQuery } from 'ufo';
+
+import type { H3Event } from 'h3';
+
+export interface GoogleAuthRes {
+  access_token: string
+  expires_in: number
+  refresh_token: string
+  scope: string
+  token_type: string
+  id_token: string
+}
+
+export interface GoogleUserRes {
+  id: string
+  email: string
+  verified_email: boolean
+  name: string
+  given_name: string
+  family_name: string
+  picture: string
+  locale: string
+}
+
+export function sendGoogleOAuthRedirect(event: H3Event) {
+  const { google, public: config } = useRuntimeConfig();
+
+  const protocol = isProduction ? 'https://' : 'http://';
+
+  return sendRedirect(event,
+    withQuery('https://accounts.google.com/o/oauth2/v2/auth', {
+      client_id: google.clientId,
+      redirect_uri: `${protocol}${config.siteOrigin}/api/oauth/google`,
+      response_type: 'code',
+      access_type: 'online',
+      prompt: 'select_account',
+      scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+    }),
+  );
+}
+
+export async function getGoogleUserWithEvent(event: H3Event) {
+  const { google, public: config } = useRuntimeConfig();
+  const code = getQuery(event).code;
+
+  if (!code)
+    throw new Error('no code was found');
+
+  const protocol = isProduction ? 'https://' : 'http://';
+
+  const auth = await $fetch<GoogleAuthRes>('https://oauth2.googleapis.com/token', {
+    method: 'POST',
+    body: {
+      code,
+      client_id: google.clientId,
+      client_secret: google.clientSecret,
+      grant_type: 'authorization_code',
+      redirect_uri: `${protocol}${config.siteOrigin}/api/oauth/google`,
+    },
+  });
+
+  const user = await $fetch<GoogleUserRes>('https://www.googleapis.com/oauth2/v2/userinfo', {
+    headers: {
+      Authorization: `${auth.token_type} ${auth.access_token}`,
+      Accept: 'application/json',
+    },
+  });
+
+  return user;
+}
