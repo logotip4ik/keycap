6137c4c96635fcc802d49e0aa3da72122235ebe7
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-07-12 18:05:19 +0300

    refactor: turn off prerendering for auth pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -99,8 +99,8 @@ export default defineNuxtConfig({
 
     '/': { prerender: true },
     '/about': { prerender: true },
-    '/login': { prerender: true },
-    '/register': { prerender: true },
+    // '/login': { prerender: true },
+    // '/register': { prerender: true },
   },
 
   modules: [
