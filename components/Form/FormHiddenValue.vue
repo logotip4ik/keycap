b56b22ded562caef47fd769a7c9f311fb0612abc
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-07-14 13:00:48 +0300

    feat: form hidden value

---
+++ components/Form/FormHiddenValue.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+interface Props {
+  id: string
+  name: string
+  type: 'checkbox' | 'text'
+  value: string | boolean
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <input
+    :id="id"
+    :name="name"
+    :type="type"
+    :value="type === 'text' ? value : undefined"
+    :checked="type === 'checkbox' ? Boolean(value) : undefined"
+    class="form__input form__input--hidden"
+  >
+</template>
+
+<style lang="scss">
+.form__input {
+  &--hidden {
+    display: none;
+  }
+}
+</style>
