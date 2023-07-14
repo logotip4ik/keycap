b543b9b65348d6296a390124c742dfb05ab57830
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-07-14 16:30:49 +0300

    feat: nav components

---
+++ components/Nav/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="nav">
+    <slot />
+  </nav>
+</template>
+
+<style lang="scss">
+.nav {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  position: absolute;
+  z-index: 10;
+  left: 50%;
+  top: calc(2.5vh + 1vw);
+
+  width: 93%;
+  max-width: 1200px;
+
+  padding: 1rem 1rem;
+
+  transform: translateX(-50%);
+}
+</style>
---
+++ components/Nav/NavSimple.vue
@@ -0,0 +1,26 @@
+<template>
+  <Nav>
+    <NavLink class="nav__link--title font-wide" to="/">
+      <img
+        src="/favicon-32x32.png"
+        alt="purple keycap"
+        class="nav__link__img"
+        fetchpriority="high"
+        decoding="async"
+        width="32"
+        height="32"
+      >
+      Keycap
+    </NavLink>
+  </Nav>
+</template>
+
+<style lang="scss">
+.nav__link {
+  &--title {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+}
+</style>
