1774cedb3391ccd40117dbb794036e29a9d29012
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-07-14 20:49:58 +0300

    feat: kysely file

---
+++ kysely/main.ts
@@ -0,0 +1,26 @@
+import { Kysely, PostgresDialect } from 'kysely';
+import { Pool } from 'pg';
+
+import type { DB } from './types';
+
+declare global {
+  // eslint-disable-next-line vars-on-top, no-var
+  var __kysely: Kysely<DB> | undefined;
+}
+
+const dialect = new PostgresDialect({
+  pool: async () => new Pool({
+    connectionString: process.env.DATABASE_URL,
+  }),
+});
+
+export function getKysely() {
+  if (!globalThis.__kysely) {
+    globalThis.__kysely = new Kysely({
+      dialect,
+      // plugins: [new DeduplicateJoinsPlugin()],
+    });
+  }
+
+  return globalThis.__kysely;
+}
