2c3e2e3f3f76fedc08e5bf9df60d0fc0a13f4b9f
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-07-24 14:05:16 +0300

    refactor: remove not used functions

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -2,9 +2,6 @@ import process from 'node:process';
 import { isProduction } from 'std-env';
 import { withLeadingSlash, withoutTrailingSlash } from 'ufo';
 
-import type { H3Event } from 'h3';
-import type { Prisma } from '@prisma/client';
-
 export function getServerUserAgent() {
   const postfix = isProduction ? '' : 'Dev';
   const serverName = process.env.SERVER_NAME || 'Keycap';
@@ -39,62 +36,4 @@ export function toBigIntId(string: string): bigint {
   catch {
     return BigInt(-1);
   }
-}
-
-export function getNoteSelectParamsFromEvent(event: H3Event): Prisma.NoteSelect {
-  const query = getQuery(event);
-
-  const isGetNoteQuery = typeof query.getNote !== 'undefined';
-  const isDetailsRequest = typeof query.details !== 'undefined';
-
-  const defaultSelects = { id: true, name: true, content: true, path: true };
-
-  if (isMethod(event, 'GET') && isDetailsRequest) {
-    return {
-      // TODO: ability to add multiple shares to one note ?
-      shares: { take: 1, select: { link: true, updatedAt: true, createdAt: true } },
-      updatedAt: true,
-      createdAt: true,
-    };
-  }
-
-  if (isMethod(event, 'PATCH')) {
-    if (isGetNoteQuery)
-      return defaultSelects;
-    else
-      return { id: true };
-  }
-
-  return defaultSelects;
-}
-
-export function getFolderSelectParamsFromEvent(event: H3Event): Prisma.FolderSelect {
-  const query = getQuery(event);
-
-  const isDetailsRequest = typeof query.details !== 'undefined';
-
-  const defaultSelects = { id: true, name: true, path: true, root: true };
-
-  if (isMethod(event, 'GET')) {
-    if (isDetailsRequest) {
-      return { updatedAt: true, createdAt: true };
-    }
-    else {
-      return {
-        ...defaultSelects,
-
-        notes: {
-          select: { id: true, name: true, path: true },
-          orderBy: { name: 'asc' },
-        },
-
-        subfolders: {
-          select: { ...defaultSelects },
-          orderBy: { name: 'asc' },
-        },
-      };
-    }
-  }
-
-  return defaultSelects;
 }
