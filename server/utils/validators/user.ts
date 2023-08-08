207f00faf9bd7f358fc57d34409c829ae97033d3
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-08-08 19:35:41 +0300

    feat: username validator and better RE

---
+++ server/utils/validators/user.ts
@@ -0,0 +1,50 @@
+import { compile, v } from 'suretype';
+
+export const usernameRE = /^[\w.\-]{3,16}$/;
+
+export const usernameSchema = v
+  .string()
+  .matches(usernameRE)
+  .minLength(3)
+  .maxLength(16)
+  .required();
+
+export const useUsernameValidation = compile(usernameSchema);
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+
+  describe('username validation', () => {
+    it('disallows spaces', () => {
+      const username = 'userna me';
+
+      const validation = useUsernameValidation(username);
+
+      expect(validation.ok).toBe(false);
+    });
+
+    it('should be valid case', () => {
+      const username = 'something';
+
+      const validation = useUsernameValidation(username);
+
+      expect(validation.ok).toBe(true);
+    });
+
+    it('allows alphanumeric values', () => {
+      const username = 'li.34t-h3s';
+
+      const validation = useUsernameValidation(username);
+
+      expect(validation.ok).toBe(true);
+    });
+
+    it('disallows empty values', () => {
+      const username = '';
+
+      const validation = useUsernameValidation(username);
+
+      expect(validation.ok).toBe(false);
+    });
+  });
+}
