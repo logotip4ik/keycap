8db51d7cd7b3dc9923503efc59a36d53816abe84
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-08-14 21:49:42 +0300

    refactor: simply auth refresh plugin

--- plugins/refresh-auth.client.ts
+++ plugins/refresh-auth.client.ts
@@ -2,7 +2,7 @@ import parseDuration from 'parse-duration';
 
 import { toBigInt } from '~/server/utils';
 
-const NEAR_HOUR = parseDuration('0.9hour')!;
+const NEAR_HOUR = parseDuration('0.9 hour')!;
 
 let prevInterval: NodeJS.Timer;
 
@@ -12,30 +12,14 @@ export default defineNuxtPlugin({
     const user = useUser();
     const route = useRoute();
 
+    refreshAuth();
     // TODO: clear interval and setUserOnClient if user logged out
-
-    watch(user, (newUser, oldUser) => {
-      // prevents scenario when initially user is not defined
-      // then fetches one, and refetches again because user changed
-      // from null to { ...user }
-      if (newUser && !oldUser)
-        return;
-
-      clearInterval(prevInterval);
-
-      refreshAuth();
-
-      if (newUser)
-        prevInterval = setInterval(refreshAuth, NEAR_HOUR);
-    }, { immediate: true });
+    prevInterval = setInterval(refreshAuth, NEAR_HOUR);
 
     function refreshAuth() {
       requestIdleCallback(async () => {
         const newUser = await $fetch('/api/user/refresh', { retry: 1 })
-          .catch(async (error) => {
-            if (error.statusCode === 401 && route.path.includes('/@'))
-              await navigateTo('/login', { replace: true });
-          });
+          .catch(() => null);
 
         const isSameUser = newUser && newUser.username === user.value?.username;
 
