943b3b18609258d6d227b5ea494d07753a715318
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-08-15 12:57:10 +0300

    fix: await get function

--- server/api/test.get.ts
+++ server/api/test.get.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async () => {
   const storage = useStorage('cache');
 
   const nowGet = performance.now();
-  const item = storage.getItem('test:oauth');
+  const item = await storage.getItem('test:oauth');
   const diffGet = performance.now() - nowGet;
 
   return { item, diffGet };
