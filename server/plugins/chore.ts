5fff5993e69c8cc8a1b80d0f41c9c239394b7843
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-08-16 17:15:17 +0300

    feat: pre connect

--- server/plugins/chore.ts
+++ server/plugins/chore.ts
@@ -21,4 +21,14 @@ Object.defineProperty(
 setSuretypeOptions({ colors: false, location: false });
 
 // TODO: test if prisma.$connect and $disconnect improves performance
-export default defineNitroPlugin(() => undefined);
+export default defineNitroPlugin((nitro) => {
+  const prisma = getPrisma();
+
+  prisma.$connect();
+
+  nitro.hooks.hookOnce('close', async () => {
+    const prisma = getPrisma();
+
+    await prisma.$disconnect();
+  });
+});
