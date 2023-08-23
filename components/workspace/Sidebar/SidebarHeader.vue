ce333dd7965eb19265fcebb5f3e637c77ffa8bd8
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-08-23 10:54:51 +0300

    feat: sidebar header

---
+++ components/workspace/Sidebar/SidebarHeader.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+import type { SidebarState } from './Sidebar.vue';
+
+interface Props { state: SidebarState; onUpdateState: (newState: SidebarState) => any };
+const props = defineProps<Props>();
+
+const user = useUser();
+</script>
+
+<template>
+  <header class="sidebar__header">
+    <!-- TODO: better accessibility -->
+    <button
+      class="sidebar__header__open-btn"
+      :class="{ 'sidebar__header__open-btn--exposed': state === 'hidden' }"
+      aria-controls="sidebar"
+      :aria-pressed="state === 'fixed'"
+      @click="onUpdateState(state === 'fixed' ? 'hidden' : 'fixed')"
+      @mouseenter="state === 'hidden' && onUpdateState('visible')"
+    >
+      <LazyIconMenuRounded class="sidebar__header__open-btn__icon" />
+    </button>
+
+    <p class="sidebar__header__username font-wide">
+      {{ user?.username }}
+    </p>
+  </header>
+</template>
+
+<style lang="scss">
+.sidebar__header {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  &__username {
+    font-size: calc(1rem + 0.25vw);
+    color: hsla(var(--text-color-hsl), 0.9);
+    text-overflow: ellipsis;
+    white-space: nowrap;
+
+    margin: 0 0 0 var(--pd-x);
+    padding: 0;
+
+    overflow: hidden;
+  }
+
+  &__open-btn {
+    flex-shrink: 0;
+    --size: min(5vw, 2.5rem);
+
+    width: var(--size);
+    height: var(--size);
+
+    appearance: none;
+    border-radius: 0.25rem;
+    border: 1px solid hsla(var(--text-color-hsl), 0.125);
+    background-color: hsla(var(--surface-color-hsl), 0.95);
+
+    cursor: pointer;
+    transition: transform var(--sidebar-tr-duration) var(--ease);
+
+    @supports (backdrop-filter: blur(1px)) {
+      border-color: transparent;
+      background-color: transparent;
+
+      backdrop-filter: blur(12px);
+    }
+
+    @media (width <= $breakpoint-tablet) {
+      --size: min(12.5vw, 3.125rem);
+    }
+
+    &:is(:hover, :focus-visible) {
+      outline: 1px solid hsla(var(--selection-bg-color-hsl), 0.25);
+
+      svg {
+        color: hsla(var(--text-color-hsl), 0.65);
+
+        transition-duration: .1s;
+      }
+    }
+
+    &[aria-pressed="true"] {
+      outline: 1px solid hsla(var(--selection-bg-color-hsl), 0.75);
+      box-shadow: 0 0 0.5rem 0 hsla(var(--selection-bg-color-hsl), 0.25);
+
+      svg {
+        color: hsla(var(--text-color-hsl), 0.8);
+
+        transition-duration: .1s;
+      }
+    }
+
+    &--exposed {
+      transform: translate(calc(var(--sidebar-width) + 1px), 1px) scale(calc(2 - var(--sidebar-hidden-scale)));
+    }
+
+    &__icon {
+      color: hsla(var(--text-color-hsl), 0.5);
+
+      width: 100%;
+      height: auto;
+
+      transition: color var(--sidebar-tr-duration) ease;
+    }
+  }
+}
+</style>
