e375d318f6076dcbfa5e45a9b0cce1cd4cf9fbb5
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-08-26 17:21:08 +0300

    feat: input for contents list item

---
+++ components/workspace/Contents/List/ContentsListItemInput.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+interface Props { item: FolderOrNote; parent: FolderWithContents }
+const props = defineProps<Props>();
+
+const name = ref<string>(props.item.name || '');
+
+function handleSubmit() {
+  if (props.item.creating) {
+    const creationName = name.value.replace(/\//g, '');
+    const createAction = creationName.length !== name.value.length ? createFolder : createNote;
+
+    createAction(creationName, props.item, props.parent);
+  }
+  else if (props.item.editing) {
+    const renameAction = 'root' in props.item ? renameFolder : renameNote;
+
+    renameAction(name.value, props.item, props.parent);
+  }
+}
+
+function handleReset() {
+  if (props.item.creating)
+    return deleteNoteFromFolder(props.item, props.parent);
+
+  const updateAction = 'root' in props.item ? updateSubfolderInFolder : updateNoteInFolder;
+
+  updateAction(props.item, { editing: false, creating: false }, props.parent);
+}
+</script>
+
+<template>
+  <form
+    class="list-item__form"
+    @submit.prevent="handleSubmit"
+    @reset.prevent="handleReset"
+  >
+    <label v-once class="list-item__form__label" for="contentsListItemInput">
+      Item name (enter "/" at the end to create folder)
+    </label>
+    <input
+      id="contentsListItemInput"
+      v-model="name"
+      class="list-item__form__input"
+      enterkeyhint="done"
+      type="text"
+      minlength="2"
+      @blur="handleReset"
+      @keydown.esc="handleReset"
+    >
+  </form>
+</template>
+
+<style lang="scss">
+.list-item__form {
+  &__label {
+    position: absolute;
+
+    width: 0;
+    height: 0;
+
+    overflow: hidden;
+    pointer-events: none;
+  }
+
+  &__input {
+    font-family: inherit;
+    color: hsla(var(--text-color-hsl), 1);
+
+    width: 100%;
+
+    padding: calc(var(--pd-y) * 0.7) calc(var(--pd-x));
+
+    appearance: none;
+    outline: 1px solid hsla(var(--selection-bg-color-hsl), 0.5);
+    outline-offset: -1px;
+    border: none;
+    border-radius: 0.225rem;
+  }
+}
+</style>
