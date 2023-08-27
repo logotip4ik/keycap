b443af6beb3088bdfbfa69488830165eea02423a
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-08-27 20:16:52 +0300

    refactor: guard recent route with route rules

--- server/middleware/3.route-rules.ts
+++ server/middleware/3.route-rules.ts
@@ -4,16 +4,6 @@ interface Rule { path: string; handler: RuleFunction }
 // true and undefined - shouldPass else - shouldDisallow
 type RuleFunction = (event: H3Event) => boolean | undefined | Promise<boolean | undefined | void>;
 
-const rules: Array<Rule> = [
-  { path: '/api/note', handler: withUserOnly },
-  { path: '/api/folder', handler: withUserOnly },
-  { path: '/api/search', handler: withUserOnly },
-  { path: '/api/user/refresh', handler: withUserOnly },
-  { path: '/api/share/note', handler: withUserOnly },
-
-  { path: '/api/oauth', handler: withoutUser },
-];
-
 function withoutUser(event: H3Event) {
   const { oauthEnabled } = useRuntimeConfig(event).public;
 
@@ -31,6 +21,17 @@ function withoutUser(event: H3Event) {
 function withUserOnly(event: H3Event) {
   return !!event.context.user;
 }
+
+const rules: Array<Rule> = [
+  { path: '/api/note', handler: withUserOnly },
+  { path: '/api/folder', handler: withUserOnly },
+  { path: '/api/search', handler: withUserOnly },
+  { path: '/api/recent', handler: withUserOnly },
+  { path: '/api/user/refresh', handler: withUserOnly },
+  { path: '/api/share/note', handler: withUserOnly },
+
+  { path: '/api/oauth', handler: withoutUser },
+];
 
 export default defineEventHandler(async (event) => {
   const rule = rules.find((rule) => event.path.startsWith(rule.path));
