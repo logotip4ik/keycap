ced205ba1b2f467c10523f95f51e144c1512f77b
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-08-28 21:08:51 +0300

    refactor: better styles for keyboard navigation

--- components/workspace/Toolbox/Recent/ToolboxRecentItem.vue
+++ components/workspace/Toolbox/Recent/ToolboxRecentItem.vue
@@ -15,10 +15,43 @@ const itemHref = computed(() => generateItemPath(props.item));
 .recent-item {
   display: block;
 
+  position: relative;
+  z-index: 1;
+
   font-size: 1.025rem;
   font-weight: 300;
   color: currentColor;
   text-decoration: underline dashed 1px hsla(var(--selection-bg-color-hsl), 1);
   text-underline-offset: 3px;
+
+  &::after {
+    content: '';
+
+    position: absolute;
+    top: 100%;
+    left: 0;
+    z-index: -1;
+
+    width: 100%;
+    height: 2px;
+
+    opacity: 0;
+    background-color: var(--task-list-indicator-color);
+
+    pointer-events: none;
+    transition: opacity .1s;
+  }
+
+  transition: border-bottom-width .1s;
+
+  &:focus-visible {
+    color: var(--text-color);
+
+    outline: none;
+
+    &::after {
+      opacity: 1;
+    }
+  }
 }
 </style>
