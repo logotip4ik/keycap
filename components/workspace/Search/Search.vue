a0d151bd090aa3f50287f1204a2975723ff2ffd9
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-08-31 14:14:12 +0300

    chore: add todos

--- components/workspace/Search/Search.vue
+++ components/workspace/Search/Search.vue
@@ -166,6 +166,7 @@ onMounted(() => {
           </p>
         </div>
 
+        <!-- TODO: rework this animation to use transition events -->
         <TransitionGroup
           v-else-if="results.length !== 0"
           ref="resultsEl"
