b0a3e0ef4271be5836c22a676ad5b9baa806e340
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-09-08 22:40:10 +0300

    refactor: hoist `new Date` to now and rename some variables

--- server/api/folder/[...path].patch.ts
+++ server/api/folder/[...path].patch.ts
@@ -33,19 +33,20 @@ export default defineEventHandler(async (event) => {
   if (!data.name)
     return { ok: true };
 
+  const now = new Date();
   const newFolderPath = makeNewItemPath(folderPath, data.name);
 
   const sqlStartsWithFolderPath = `${folderPath}/%`;
+  const sqlFolderPathRegexp = `^${folderPath.replace(/\//g, '\/')}\/`;
 
-  const replaceRegexp = `^${folderPath.replace(/\//g, '\/')}\/`;
   const replaceValue = `${newFolderPath}/`;
 
   timer.start('db');
   const res = await prisma.$transaction([
-    prisma.$queryRaw`UPDATE "Folder" SET "name" = ${data.name}, "path" = ${newFolderPath}, "updatedAt" = ${new Date()} WHERE ("Folder"."ownerId" = ${user.id} AND "Folder"."path"::text = ${folderPath})`,
+    prisma.$queryRaw`UPDATE "Folder" SET "name" = ${data.name}, "path" = ${newFolderPath}, "updatedAt" = ${now} WHERE ("Folder"."ownerId" = ${user.id} AND "Folder"."path"::text = ${folderPath})`,
 
-    prisma.$queryRaw`UPDATE "Note" SET "path" = regexp_replace("path"::text, ${replaceRegexp}, ${replaceValue}, 'c'), "updatedAt" = ${new Date()} WHERE ("Note"."ownerId" = ${user.id} AND "Note"."path"::text LIKE ${sqlStartsWithFolderPath})`,
-    prisma.$queryRaw`UPDATE "Folder" SET "path" = regexp_replace("path"::text, ${replaceRegexp}, ${replaceValue}, 'c'), "updatedAt" = ${new Date()} WHERE ("Folder"."ownerId" = ${user.id} AND "Folder"."path"::text LIKE ${sqlStartsWithFolderPath})`,
+    prisma.$queryRaw`UPDATE "Note" SET "path" = regexp_replace("path"::text, ${sqlFolderPathRegexp}, ${replaceValue}, 'c'), "updatedAt" = ${now} WHERE ("Note"."ownerId" = ${user.id} AND "Note"."path"::text LIKE ${sqlStartsWithFolderPath})`,
+    prisma.$queryRaw`UPDATE "Folder" SET "path" = regexp_replace("path"::text, ${sqlFolderPathRegexp}, ${replaceValue}, 'c'), "updatedAt" = ${now} WHERE ("Folder"."ownerId" = ${user.id} AND "Folder"."path"::text LIKE ${sqlStartsWithFolderPath})`,
   ]).catch(async (err) => {
     await event.context.logger.error({ err }, 'rename folder failed');
   });
