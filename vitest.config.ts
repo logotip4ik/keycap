da573984f3540aafd19255a734d96d53e363adbd
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-09-29 22:23:55 +0300

    chore: remove unused error supression

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,6 @@
 import { defineVitestConfig as defineConfig } from 'nuxt-vitest/config';
 
 export default defineConfig({
-  // @ts-expect-error probably because of vitest v0.34.0, but it still works
   test: {
     setupFiles: ['./server/setup.nitro.ts'],
 
