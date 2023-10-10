1a207b68c3e5d0cbc3421333a9b97d7cfd61a56f
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-10-11 00:00:33 +0300

    refactor: migrate to eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import antfu from '@antfu/eslint-config';
+import regexp from 'eslint-plugin-regexp';
+
+const ignores = [
+  'data',
+  'node_modules',
+  '.nuxt',
+  '.output',
+  '.vscode',
+  '.yarn/',
+  'archive',
+  'prisma/migrations',
+  'public',
+  'docker-compose.*',
+  'package.json',
+  '*.lock',
+  'tsconfig.json',
+  'vercel.json',
+  'dist',
+  'benchmarks',
+];
+
+export default antfu({
+  stylistic: {
+    quotes: 'single',
+  },
+
+  rules: {
+    'style/semi': ['error', 'always'],
+    'ts/array-type': ['error', { default: 'generic' }],
+    'ts/no-shadow': 'off',
+
+    'arrow-parens': ['error', 'always'],
+    'curly': ['error', 'multi-or-nest', 'consistent'],
+    'antfu/if-newline': 'off',
+    'import/no-nodejs-modules': 'off',
+    'yaml/no-empty-mapping-value': 'off',
+    'node/prefer-global/process': ['error', 'always'],
+  },
+}, {
+  ...regexp.configs.recommended,
+  plugins: { regexp },
+}, {
+  ignores,
+});
