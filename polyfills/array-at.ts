28469acacd62db231b049e7f8546d09351f75618
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-10-15 15:06:40 +0300

    refactor: pollyfill `.at` only when it is not defined

--- polyfills/array-at.ts
+++ polyfills/array-at.ts
@@ -1,20 +1,22 @@
-/* @see https://github.com/tc39/proposal-relative-indexing-method#polyfill */
-function at(this: Array<any> | string, n: number) {
-  n = Math.trunc(n) || 0;
-  if (n < 0) n += this.length;
-  if (n < 0 || n >= this.length) return undefined;
-  return this[n];
-}
+if (!Array.prototype.at) {
+  /* @see https://github.com/tc39/proposal-relative-indexing-method#polyfill */
+  function at(this: Array<any> | string, n: number) {
+    n = Math.trunc(n) || 0;
+    if (n < 0) n += this.length;
+    if (n < 0 || n >= this.length) return undefined;
+    return this[n];
+  }
 
-for (const C of [Array, String]) {
-  Object.defineProperty(
-    C.prototype,
-    'at',
-    {
-      value: at,
-      writable: true,
-      enumerable: false,
-      configurable: true,
-    },
-  );
+  for (const C of [Array, String]) {
+    Object.defineProperty(
+      C.prototype,
+      'at',
+      {
+        value: at,
+        writable: true,
+        enumerable: false,
+        configurable: true,
+      },
+    );
+  }
 }
