d0d1ce5ed6ac91c473d1f8cc2d82d05f0cd6cfe1
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-10-23 13:58:26 +0300

    chore: supres typescript error

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { defineVitestConfig as defineConfig } from 'nuxt-vitest/config';
 
 export default defineConfig({
+  // @ts-expect-error just following the docs and it works ?
   test: {
     setupFiles: ['./server/setup.nitro.ts'],
 
