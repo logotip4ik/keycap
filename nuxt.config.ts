269036a02df5055959c86e5840c63ea8875b4a2f
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-10-24 11:48:23 +0300

    refactor: try settings isr to true on `api/info` route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -114,6 +114,7 @@ export default defineNuxtConfig({
     '/_nuxt/**': { headers: getHeaders('assets') },
 
     '/api/**': { headers: getHeaders('api') },
+    '/api/info': { isr: true },
 
     // this would be great https://github.com/unjs/nitro/issues/603#issuecomment-1415826732
     '/view/**': { isr: parseDuration('15 minutes', 'second') },
