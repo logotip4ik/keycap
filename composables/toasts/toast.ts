7b53ec79031cd49b0a4f0968991ed14acc1f91dc
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-11-16 22:00:09 +0200

    refactor: split `toasts` and create toast class instead of plain object

---
+++ composables/toasts/toast.ts
@@ -0,0 +1,66 @@
+import type { ShallowRef } from 'vue';
+import type { MaybeRefOrGetter } from '@vueuse/core';
+
+export type ToastType = 'info' | 'loading';
+
+export interface ToastButton {
+  text: MaybeRefOrGetter<string>
+  onClick: (instance: ToastInstance) => any
+}
+
+export interface ToastInstance {
+  id: number
+  // TODO: add support for html ?
+  // probably should support rendering function instead of html
+  message: MaybeRef<string>
+  /**
+   * should be between 0 and 100
+   * - 0 will float freely to top with other toast
+   * - 100 will be fixed at the bottom
+   * @default 0
+   */
+  priority: number
+  /**
+   * in milliseconds
+   * @default 6000
+   */
+  duration: number
+  /** @default 'info' */
+  type: ToastType
+  el: ShallowRef<HTMLOutputElement | null>
+  buttons: Array<ToastButton>
+  remove: () => void
+}
+
+export interface ToastsInstanceOptions {
+  id: ToastInstance['id']
+  message: ToastInstance['message']
+  remove: () => void
+  priority?: ToastInstance['priority']
+  duration?: ToastInstance['duration']
+  type?: ToastInstance['type']
+  buttons?: ToastInstance['buttons']
+}
+
+export class Toast implements ToastInstance {
+  public readonly id: number;
+  public readonly message: MaybeRef<string>;
+  public readonly priority: number;
+  public readonly duration: number;
+  public readonly type: ToastType; ;
+  public readonly el: ShallowRef<HTMLOutputElement | null>;
+  public readonly buttons: Array<ToastButton>;
+  public readonly remove: () => void;
+
+  constructor(opts: ToastsInstanceOptions) {
+    this.id = opts.id;
+    this.message = opts.message;
+    this.remove = opts.remove;
+
+    this.priority = opts.priority ?? 0;
+    this.duration = opts.duration ?? 6000;
+    this.type = opts.type ?? 'info';
+    this.buttons = opts.buttons ?? [];
+    this.el = shallowRef(null);
+  }
+}
---
+++ composables/toasts/toaster.ts
@@ -0,0 +1,56 @@
+import type { ShallowRef } from 'vue';
+
+export type RefToastInstance = ShallowRef<ToastInstance | null | undefined>;
+export type ToastUserOptions = Partial<Omit<ToastInstance, 'id' | 'message' | 'remove' | 'el' | 'delete'>>;
+
+const toasts = shallowRef<Array<ToastInstance>>([]);
+export const useToasts = () => toasts;
+
+function createToastRef(message: ToastInstance['message'], options?: ToastUserOptions & { delay?: number }): RefToastInstance {
+  const timeout: { value: NodeJS.Timeout | null } = { value: null };
+
+  const toast = createToast({ ...options, message, timeout });
+
+  const addToastToQueue = () => {
+    timeout.value = null;
+    toasts.value = toasts.value.concat(toast.value!);
+  };
+
+  if (options?.delay)
+    timeout.value = setTimeout(addToastToQueue, options.delay);
+  else
+    addToastToQueue();
+
+  return toast;
+};
+
+export function useToaster() {
+  return createToastRef;
+}
+
+function createToast(options: ToastUserOptions & { message: ToastInstance['message']; timeout: { value: NodeJS.Timeout | null } }): RefToastInstance {
+  if (unref(options.message).trim() === '')
+    throw new Error('no message was provided');
+
+  const toastId = Math.floor(Math.random() * 9999999);
+
+  // TODO: refactor this to class
+  const toast: RefToastInstance = shallowRef(
+    new Toast({
+      ...options,
+
+      id: toastId,
+      message: options.message,
+      remove: () => {
+        if (options.timeout.value)
+          clearTimeout(options.timeout.value);
+        else
+          toasts.value = toasts.value.filter((toast) => toast.id !== toastId);
+
+        toast.value = undefined;
+      },
+    }),
+  );
+
+  return toast;
+}
