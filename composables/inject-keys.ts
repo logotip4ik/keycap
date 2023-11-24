55dc459b8ace9aa2826d40beb534081f3e447caa
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-11-24 21:30:35 +0200

    feat: provide wrapper composabels for injections

--- composables/inject-keys.ts
+++ composables/inject-keys.ts
@@ -3,3 +3,14 @@ import type { ComputedRef, InjectionKey } from 'vue';
 export const IsSmallScreenKey = Symbol(import.meta.dev ? '__is-small-screen-key__' : '') as InjectionKey<boolean>;
 export const IsNoteNameEmptyKey = Symbol(import.meta.dev ? '__is-note-name-empty__' : '') as InjectionKey<ComputedRef<boolean>>;
 export const IsFirefoxKey = Symbol(import.meta.dev ? '__is-firefox__' : '') as InjectionKey<boolean>;
+
+export function getIsSmallScreen() {
+  return inject(IsSmallScreenKey);
+}
+
+export function getIsNoteEmpty() {
+  return inject(IsNoteNameEmptyKey);
+}
+export function getIsFirefox() {
+  return inject(IsFirefoxKey);
+}
