592d1d0e201ea371cc4f1aefe7620f048967358a
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-11-26 17:52:48 +0200

    fix: pwa could be undefined

--- utils/sw-controller.ts
+++ utils/sw-controller.ts
@@ -4,19 +4,19 @@ export function updateServiceWorker() {
   const { $pwa: pwa } = useNuxtApp();
   const createToast = useToaster();
 
-  watch(() => pwa.offlineReady, (ready) => {
+  watch(() => pwa?.offlineReady, (ready) => {
     if (ready)
       createToast('Phewww, now you can work offline', { delay: 550 });
   }, { immediate: true });
 
-  watch(() => pwa.needRefresh, (needRefresh) => {
+  watch(() => pwa?.needRefresh, (needRefresh) => {
     if (needRefresh) {
       createToast('Psss... We have some updates', {
         priority: 10,
         duration: parseDuration('25 seconds')!,
         delay: 550,
         buttons: [
-          { text: 'refresh now', onClick: () => pwa.updateServiceWorker() },
+          { text: 'refresh now', onClick: () => pwa?.updateServiceWorker() },
           { text: 'nahh, not now', onClick: (t) => t.remove() },
         ],
       });
