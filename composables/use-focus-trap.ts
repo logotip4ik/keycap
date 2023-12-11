5a8a3d8af890841068910db1418112ea9a51153f
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-12-11 13:55:50 +0200

    fix: do handle focus trapping also for small screens

--- composables/use-focus-trap.ts
+++ composables/use-focus-trap.ts
@@ -1,9 +1,7 @@
 const TABBABLE_ELs = 'input:not([disabled]), select:not([disabled]), textarea:not([disabled]), a[href], button:not([disabled]), [tabindex="0"], audio[controls], video[controls], [contenteditable]:not([contenteditable="false"])';
 
 export function useFocusTrap(el: MaybeRef<HTMLElement | null | undefined>) {
-  const isSmallScreen = getIsSmallScreen();
-
-  if (import.meta.server || isSmallScreen)
+  if (import.meta.server)
     return;
 
   let lastFocusedEl: HTMLElement | undefined;
