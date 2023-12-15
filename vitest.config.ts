6b1cc66615c3c43f3e114ee9b05fb9f8230e908e
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-12-15 15:38:39 +0200

    refactor: use vitest default exclude variable

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import { defaultExclude } from 'vitest/config';
 import { defineVitestConfig as defineConfig } from 'nuxt-vitest/config';
 
 export default defineConfig({
@@ -11,11 +12,7 @@ export default defineConfig({
     ],
 
     exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/cypress/**',
-      '**/.{idea,git,cache,output,temp}/**',
-      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      ...defaultExclude,
       '**/data/**',
     ],
   },
