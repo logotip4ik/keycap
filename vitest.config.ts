eeeec13cdcec0dd3ed3075cbf9f31446790a7ac5
Daniel Roe <daniel@roe.dev>
2023-12-15 16:04:26 +0000

    chore: remove error expectation

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defaultExclude } from 'vitest/config';
 import { defineVitestConfig as defineConfig } from '@nuxt/test-utils/config';
 
 export default defineConfig({
-  // @ts-expect-error just following the docs and it works ?
   test: {
     setupFiles: ['./server/test/stub-nitro.ts'],
 
