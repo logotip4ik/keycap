86d9d312827065ef823ec2a4c13b3b4b85d20136
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-12-27 13:21:00 +0200

    refactor: export transition with fade effect to component

---
+++ components/With/FadeTransition.vue
@@ -0,0 +1,28 @@
+<template>
+  <Transition name="fade">
+    <slot />
+  </Transition>
+</template>
+
+<style lang="scss">
+.fade-enter-active {
+  transition: opacity 0.3s * 2 ease;
+
+  &.fast-fade {
+    transition-duration: 0.3s;
+  }
+
+  &.skeleton {
+    transition-delay: 0.4s;
+  }
+}
+
+.fade-leave-active {
+  display: none !important;
+  transition-duration: 0s;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+</style>
