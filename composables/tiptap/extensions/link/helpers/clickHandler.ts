de650cf1cb74a349d60da9ad2a4b4eba52b85145
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2024-01-04 23:01:36 +0200

    refactor: inner links

--- composables/tiptap/extensions/link/helpers/clickHandler.ts
+++ composables/tiptap/extensions/link/helpers/clickHandler.ts
@@ -1,4 +1,3 @@
-import { getAttributes } from '@tiptap/core';
 import { Plugin, PluginKey } from '@tiptap/pm/state';
 
 import type { MarkType } from '@tiptap/pm/model';
@@ -7,7 +6,7 @@ interface ClickHandlerOptions {
   type: MarkType
 };
 
-export function clickHandler(options: ClickHandlerOptions): Plugin {
+export function clickHandler(_options: ClickHandlerOptions): Plugin {
   return new Plugin({
     key: new PluginKey('handleClickLink'),
     props: {
@@ -26,14 +25,22 @@ export function clickHandler(options: ClickHandlerOptions): Plugin {
         if (!els.find((value) => value.nodeName === 'A'))
           return false;
 
-        const attrs = getAttributes(view.state, options.type.name);
         const link = event.target as HTMLLinkElement;
 
-        const href = link?.href ?? attrs.href;
-        const target = link?.target ?? attrs.target;
+        // TODO: add auto complete for inner links
+        if (!link)
+          return false;
 
-        if (link && href) {
-          window.open(href, target);
+        const href = link.getAttribute('href');
+        const target = link.getAttribute('target');
+
+        if (href) {
+          const innerUrl = getInnerUrl(href);
+
+          if (innerUrl)
+            navigateTo(innerUrl.pathname);
+          else
+            window.open(href, target || undefined);
 
           return true;
         }
@@ -42,4 +49,18 @@ export function clickHandler(options: ClickHandlerOptions): Plugin {
       },
     },
   });
+}
+
+function getInnerUrl(link: string) {
+  const url = new URL(link);
+
+  if (window.location.origin !== url.origin)
+    return;
+
+  const user = useUser();
+
+  if (!user.value || !url.pathname.startsWith(`/@${user.value.username}`))
+    return;
+
+  return url;
 }
