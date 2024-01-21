4546d202d9be975f9737c38b5779502bd2613f7d
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2024-01-21 13:29:05 +0200

    fix: don't remove state cookie until user finished authentication process

--- server/utils/oauth/validation.ts
+++ server/utils/oauth/validation.ts
@@ -1,32 +1,24 @@
 import type { H3Event } from 'h3';
+import type { QueryObject } from 'ufo';
 
-const providersMap = {
-  '/api/oauth/github': OAuthProvider.GitHub,
-  '/api/oauth/google': OAuthProvider.Google,
-};
+export async function assertNoOAuthErrors(event: H3Event, _query?: QueryObject) {
+  // Can't delete the cookie here, because user might need to enter a username
+  // which is done later, with separate check
+  // deleteCookie(event, 'state');
 
-export async function assertNoOAuthErrors(event: H3Event) {
-  deleteCookie(event, 'state');
-
-  const pathWithoutQuery = event.path.split('?')[0];
-
-  const provider = providersMap[pathWithoutQuery as keyof typeof providersMap];
-
-  if (!provider) {
-    await event.context.logger.error({ msg: 'undefined provider' });
-
-    throw createError({ statusCode: 418, message: 'i a coffeepot' });
-  }
-
-  const query = getQuery(event);
+  const query = _query || getQuery(event)!;
 
   if (query.error) {
+    deleteCookie(event, 'state');
+
     await event.context.logger.error({ err: new Error(query.error.toString()), msg: 'oauth failed' });
 
     throw createError({ statusCode: 418, message: decodeURIComponent(query.error.toString()) });
   }
 
-  if (typeof query.state === 'string' && query.state !== getCookie(event, 'state')) {
+  if (typeof query.state !== 'string' || query.state !== getCookie(event, 'state')) {
+    deleteCookie(event, 'state');
+
     const identifier = getRequestIP(event, { xForwardedFor: true });
 
     await event.context.logger.error({ msg: 'someone is messing with authentication', identifier });
