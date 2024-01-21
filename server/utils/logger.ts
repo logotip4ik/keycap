35d3a8baf5819c6f8a14c8867e268b3a5dc34dad
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2024-01-21 12:18:40 +0200

    refactor: simplify log function

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -1,12 +1,17 @@
 import type { H3Event } from 'h3';
 
-export type LoggerData = Record<string, unknown | undefined>;
-
-export const LOG_LEVEL = {
-  info: 'info',
-  warn: 'warn',
-  error: 'error',
-} as const;
+export enum LogLevel {
+  Info = 'info',
+  Warn = 'warn',
+  Error = 'error',
+}
+export type LoggerData = Record<string, unknown | undefined> & { msg?: string | undefined };
+export interface Logger {
+  log(level: LogLevel, data: LoggerData): Promise<void>
+  error(data: LoggerData | string): Promise<void>
+  warn(data: LoggerData | string): Promise<void>
+  info(data: LoggerData | string): Promise<void>
+}
 
 let client: typeof $fetch | undefined;
 
@@ -14,7 +19,7 @@ export function createLogger(event: H3Event) {
   const { axiom } = useRuntimeConfig();
   const [path, query] = event.path.split('?');
 
-  const additionalData: LoggerData = {
+  const baseData: LoggerData = {
     nitro: true,
     env: process.env.VERCEL_ENV || 'development',
 
@@ -23,45 +28,32 @@ export function createLogger(event: H3Event) {
     username: event.context.user?.username,
   };
 
-  const log = baseLog.bind(undefined, axiom, additionalData);
-
-  return {
-    log,
-
-    async error(data: LoggerData | string, message?: string) {
-      data = typeof data === 'string' ? { msg: data } : data;
-
-      if (message)
-        data.msg = message;
-
-      await log(LOG_LEVEL.error, data);
+  const logger: Logger = {
+    async log(level: LogLevel, data: LoggerData) {
+      await baseLog(axiom, baseData, level, data);
     },
 
-    async warn(data: LoggerData | string, message?: string) {
-      data = typeof data === 'string' ? { msg: data } : data;
-
-      if (message)
-        data.msg = message;
-
-      await log(LOG_LEVEL.warn, data);
+    async error(data: LoggerData | string) {
+      await logger.log(LogLevel.Error, typeof data === 'string' ? { msg: data } : data);
     },
 
-    async info(data: LoggerData | string, message?: string) {
-      data = typeof data === 'string' ? { msg: data } : data;
+    async warn(data: LoggerData | string) {
+      await logger.log(LogLevel.Warn, typeof data === 'string' ? { msg: data } : data);
+    },
 
-      if (message)
-        data.msg = message;
-
-      await log(LOG_LEVEL.info, data);
+    async info(data: LoggerData | string) {
+      await logger.log(LogLevel.Info, typeof data === 'string' ? { msg: data } : data);
     },
   };
+
+  return logger;
 }
 
 async function baseLog(
   axiom: ReturnType<typeof useRuntimeConfig>['axiom'],
   baseData: LoggerData,
-  level: keyof typeof LOG_LEVEL,
-  customData: LoggerData,
+  level: 'info' | 'warn' | 'error',
+  customData: LoggerData = {},
 ) {
   const data = Object.assign({}, baseData, customData);
 
