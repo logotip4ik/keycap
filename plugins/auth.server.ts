ac8f29c387a468f425120e1be60a15af7a87c184
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-01-27 14:42:22 +0200

    refactor: use already resolved user from nitro's middleware

--- plugins/auth.server.ts
+++ plugins/auth.server.ts
@@ -1,5 +1,3 @@
-import { getUserFromEvent } from '~/server/utils/auth';
-
 export default defineNuxtPlugin({
   parallel: true,
   env: { islands: false },
@@ -7,7 +5,8 @@ export default defineNuxtPlugin({
     const event = useRequestEvent();
     const user = useUser();
 
+    // We can reuse already checked user from nitro's middleware.
     // @ts-expect-error devalue will still stringify this
-    user.value = await getUserFromEvent(event);
+    user.value = event.context.user;
   },
 });
