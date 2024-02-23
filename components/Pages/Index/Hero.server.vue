ad884f74d5ff1a26ef77e4a4113ab486db758e21
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-02-23 19:28:37 +0200

    chore: remove second div wrapper

--- components/Pages/Index/Hero.server.vue
+++ components/Pages/Index/Hero.server.vue
@@ -3,26 +3,24 @@ const { build } = useRuntimeConfig().public;
 </script>
 
 <template>
-  <div>
-    <div class="container">
-      <WithBlob v-slot="props">
-        <header class="index__header" v-bind="props">
-          <small class="index__header__alert">
-            <LazyIconWarning v-once class="index__header__alert__icon" />
-            {{ build.version }} (in development)
-          </small>
+  <div class="container">
+    <WithBlob v-slot="props">
+      <header class="index__header" v-bind="props">
+        <small class="index__header__alert">
+          <LazyIconWarning v-once class="index__header__alert__icon" />
+          {{ build.version }} (in development)
+        </small>
 
-          <h1 class="index__header__title font-wide">
-            Keycap
-          </h1>
+        <h1 class="index__header__title font-wide">
+          Keycap
+        </h1>
 
-          <p class="index__header__subtitle">
-            Another note taking webapp ❤.
-            <br>
-            Simple, fast and purple.
-          </p>
-        </header>
-      </WithBlob>
-    </div>
+        <p class="index__header__subtitle">
+          Another note taking webapp ❤.
+          <br>
+          Simple, fast and purple.
+        </p>
+      </header>
+    </WithBlob>
   </div>
 </template>
