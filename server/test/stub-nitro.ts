8366b8f9bb4dc3f545d13c0c3288f08a2a487338
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-02-25 16:51:37 +0200

    chore: remove not needed import mock

--- server/test/stub-nitro.ts
+++ server/test/stub-nitro.ts
@@ -2,4 +2,3 @@ import { vitest } from 'vitest';
 
 vitest.stubGlobal('defineEventHandler', (func: any) => func);
 vitest.stubGlobal('defineNitroPlugin', (e: any) => e);
-vitest.mock('#resolved-paths', () => ({ publicPath: '' }));
