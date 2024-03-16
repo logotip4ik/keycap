b6202f8334180d44d2c0a59b03e50a9e865e4125
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-03-16 14:37:52 +0200

    feat: install kysely and drivers

---
+++ kysely/db/types.ts
@@ -0,0 +1,61 @@
+import type { ColumnType } from "kysely";
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+
+export const SocialAuth = {
+    Google: "Google",
+    GitHub: "GitHub"
+} as const;
+export type SocialAuth = (typeof SocialAuth)[keyof typeof SocialAuth];
+export type Folder = {
+    id: Generated<string>;
+    name: string;
+    path: string;
+    root: Generated<boolean>;
+    parentId: string | null;
+    ownerId: string;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+};
+export type Note = {
+    id: Generated<string>;
+    name: string;
+    content: string | null;
+    path: string;
+    ownerId: string;
+    parentId: string;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+};
+export type OAuth = {
+    id: string;
+    type: SocialAuth;
+    userId: string;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+};
+export type Share = {
+    id: Generated<string>;
+    link: string;
+    noteId: string;
+    ownerId: string;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+};
+export type User = {
+    id: Generated<string>;
+    username: string;
+    email: string;
+    password: string | null;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+};
+export type DB = {
+    Folder: Folder;
+    Note: Note;
+    OAuth: OAuth;
+    Share: Share;
+    User: User;
+};
