f55b005e455e5656ed459aef9071ed5fa40e8233
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-03-16 14:38:27 +0200

    chore: function for getting kysely instance

---
+++ kysely/kysely.ts
@@ -0,0 +1,24 @@
+import { Kysely } from 'kysely';
+import { PostgresJSDialect } from 'kysely-postgres-js';
+import postgres from 'postgres';
+
+import type { DB as Database } from './db/types';
+
+export function getKysely() {
+  if (!globalThis.__kysely) {
+    if (!process.env.DATABASE_URL)
+      throw new Error('DATABASE_URL must be defined');
+
+    globalThis.__kysely = new Kysely<Database>({
+      dialect: new PostgresJSDialect({
+        postgres: postgres(process.env.DATABASE_URL),
+      }),
+    });
+  }
+
+  return globalThis.__kysely;
+}
+
+declare global {
+  var __kysely: Kysely<Database>;
+}
