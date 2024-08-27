53b6f2600c0e211a83b543351db171e72a7723d9
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-08-27 23:43:33 +0300

    feat: add footer

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@ definePageMeta({
   middleware: ['redirect-dashboard'],
 });
 
-useHead({
+useServerHead({
   link: [
     { rel: 'preload', as: 'image', href: '/editor-wide.webp', media: '(prefers-color-scheme: light)' },
     { rel: 'preload', as: 'image', href: '/editor-wide-dark.webp', media: '(prefers-color-scheme: dark)' },
@@ -33,6 +33,7 @@ const more = [
   },
 ];
 
+const { build } = useRuntimeConfig().public;
 const editorImgWrapperEl = shallowRef<HTMLDivElement | null>(null);
 
 function animateImgPos(
@@ -134,6 +135,37 @@ onMounted(() => {
         </ol>
       </section>
     </main>
+
+    <footer class="footer font-wide">
+      <div class="footer__wrapper">
+        <div class="footer__row">
+          <p class="footer__title">
+            <img
+              src="/logo.webp"
+              alt="purple keycap"
+              class="footer__title__img"
+              decoding="async"
+              width="48"
+              height="48"
+              fetchpriority="low"
+              loading="lazy"
+            >
+            Keycap © 2024
+          </p>
+
+          <p class="footer__version">
+            {{ build.version }}
+          </p>
+        </div>
+
+        <div class="footer__row">
+          <p />
+          <p class="footer__headline">
+            Built to be used 💜
+          </p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -149,7 +181,7 @@ onMounted(() => {
 
     &__text {
       color: hsla(var(--text-color-hsl), 0.9);
-      font-size: calc(1.5rem + 1.75vw);
+      font-size: min(calc(1.5rem + 1.75vw), 3.5rem);
       text-wrap: balance;
       text-align: center;
       line-height: 1.2;
@@ -317,6 +349,77 @@ onMounted(() => {
           }
         }
       }
+    }
+  }
+}
+
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+
+  padding-top: 4rem;
+
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+
+    width: 93vw;
+    max-width: 1200px;
+
+    padding: 4rem 2rem;
+
+    border: 1px solid var(--task-list-indicator-color);
+    border-top-left-radius: 0.5rem;
+    border-top-right-radius: 0.5rem;
+    background-color: hsla(var(--text-color-hsl), 0.025);
+  }
+
+  &__row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 0.5rem;
+
+    & + & {
+      margin-top: 0.5rem;
+    }
+
+    @media screen and (max-width: $breakpoint-tablet) {
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+    }
+  }
+
+  &__version,
+  &__headline {
+    text-wrap: nowrap;
+
+    margin: 0;
+
+    opacity: 0.7;
+  }
+
+  &__title {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 0.5ch;
+
+    font-size: min(1.25rem + 0.5vw, 1.75rem);
+    text-wrap: nowrap;
+
+    margin: 0;
+    opacity: 0.9;
+
+    &__img {
+      display: block;
+
+      width: 1.75ch;
+      height: auto;
+
+      transform: translateY(1.5%);
     }
   }
 }
