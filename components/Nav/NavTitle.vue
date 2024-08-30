65969dc0c29154cc39c547c8ad2b3203f5fabc60
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-08-30 21:53:08 +0300

    chore: remove not needed img wrapper

--- components/Nav/NavTitle.vue
+++ components/Nav/NavTitle.vue
@@ -3,18 +3,16 @@
 
 <template>
   <NavLink class="nav__link--title font-wide" to="/">
-    <div class="nav__link__img__wrapper">
-      <img
-        src="/logo.webp"
-        alt="purple keycap"
-        class="nav__link__img nav__link__img--title"
-        decoding="async"
-        width="48"
-        height="48"
-        fetchpriority="low"
-        loading="lazy"
-      >
-    </div>
+    <img
+      src="/logo.webp"
+      alt="purple keycap"
+      class="nav__link__img nav__link__img--title"
+      decoding="async"
+      width="48"
+      height="48"
+      fetchpriority="low"
+      loading="lazy"
+    >
 
     Keycap
   </NavLink>
@@ -31,23 +29,18 @@
   }
 
   &__img {
+    display: block;
+
+    width: 1.5ch;
+    height: auto;
+
+    margin-bottom: 0;
+    margin-right: 0.5rem;
+
     user-select: none;
 
-    &__wrapper {
-      --img-width: 1.5ch;
-
-      position: relative;
-      z-index: 1;
-    }
-
-    &--title {
-      width: var(--img-width);
-
-      margin-bottom: 0;
-
-      @media (prefers-reduced-motion: no-preference) {
-        animation: press 10s 5s cubic-bezier(0.34, 1.56, 0.64, 1) infinite;
-      }
+    @media (prefers-reduced-motion: no-preference) {
+      animation: press 10s 5s cubic-bezier(0.34, 1.56, 0.64, 1) infinite;
     }
   }
 }
