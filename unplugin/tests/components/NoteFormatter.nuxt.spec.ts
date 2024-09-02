2ca18a78fd3c9df66c9baa18a9a0f818c289421e
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-09-02 17:44:07 +0300

    test: add note-formatter link behaivor check

---
+++ unplugin/tests/components/NoteFormatter.nuxt.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, it } from 'vitest';
+import { renderSuspended } from '@nuxt/test-utils/runtime';
+import { fireEvent } from '@testing-library/vue';
+
+import type { Editor } from '@tiptap/core';
+
+import { WorkspaceNoteFormatter as NoteFormatter } from '#components';
+
+describe('component NoteFormatter', () => {
+  it('should get and show correct link from editor', async () => {
+    const currentLink = 'https://keycap.io';
+
+    const editor = {
+      state: { selection: new Selection() },
+      isActive: (type: string) => type === 'link',
+      getAttributes: () => ({ href: currentLink }),
+    } as unknown as Editor;
+
+    const component = await renderSuspended(NoteFormatter, {
+      props: {
+        editor,
+      },
+      global: {
+        stubs: { transition: false },
+      },
+    });
+
+    const linkButton = await component.findByLabelText('toggle link');
+
+    await fireEvent.click(linkButton);
+
+    const linkInput = await component.findByRole('textbox') as HTMLInputElement;
+
+    expect(linkInput.value).toBe(currentLink);
+  });
+});
