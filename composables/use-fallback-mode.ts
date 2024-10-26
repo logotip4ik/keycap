43f380a6f9d10b9e67cf1d57c30c7e57521000fd
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-10-26 20:12:38 +0300

    fix: memory leak in fallback mode composable

--- composables/use-fallback-mode.ts
+++ composables/use-fallback-mode.ts
@@ -1,20 +1,24 @@
-let fallbackMode: Ref<boolean> | undefined;
+import proxy from 'unenv/runtime/mock/proxy';
 
 export function useFallbackMode() {
   if (import.meta.server) {
-    return ref(false);
+    return proxy;
   }
 
-  if (!fallbackMode) {
-    fallbackMode = ref(false);
+  const fallback = ref(!window.navigator.onLine);
+  const offs = [
+    on(window, 'online', () => {
+      fallback.value = false;
+    }),
+    on(window, 'offline', () => {
+      fallback.value = true;
+    }),
+  ];
 
-    mountListeners(fallbackMode);
-  }
+  onScopeDispose(() => {
+    invokeArrayFns(offs);
+    offs.length = 0;
+  });
 
-  return fallbackMode;
-}
-
-function mountListeners(fallback: Ref<boolean>) {
-  on(window, 'online', () => fallback.value = true);
-  on(window, 'offline', () => fallback.value = true);
+  return fallback;
 }
