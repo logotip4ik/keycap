32cd4628997ada80087d33f5c742067cf7a01b11
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2025-01-01 21:17:23 +0200

    chore: remove year from keycap footer copyright

--- app/components/Workspace/Toolbox/ToolboxFooter.server.vue
+++ app/components/Workspace/Toolbox/ToolboxFooter.server.vue
@@ -27,7 +27,7 @@ function isRelative(url: string) {
         fetchpriority="low"
         loading="lazy"
       >
-      Keycap © 2024 <span class="toolbox__footer__title__version">{{ build.version }}</span>
+      Keycap © <span class="toolbox__footer__title__version">{{ build.version }}</span>
     </p>
 
     <ul class="toolbox__footer__links">
@@ -65,7 +65,7 @@ function isRelative(url: string) {
     justify-content: flex-start;
     align-items: center;
 
-    font-size: 0.925rem;
+    font-size: 1rem;
     font-weight: 300;
     color: hsla(var(--text-color-hsl), 0.65);
 
