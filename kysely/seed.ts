30e8bb7d370acab75e6e7ae8c613b4a5f93c1961
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2025-01-25 11:47:37 +0200

    chore: allow creating lots of random users

--- kysely/seed.ts
+++ kysely/seed.ts
@@ -28,7 +28,7 @@ async function createUser() {
 
 async function createRootFolder(user: { id: string, username: string }) {
   const rootFolder = {
-    id: '1',
+    id: faker.number.bigInt().toString(),
     name: `${user.username}'s workspace`,
     path: `/${user.username}`,
     root: true,
@@ -56,13 +56,39 @@ async function createNotes(user: { id: string }, folder: { id: string, path: str
   await sql`insert into ${sql('Note')} ${sql(notes)}`;
 }
 
+async function createRandomUser() {
+  const user = {
+    id: faker.number.bigInt().toString(),
+    username: faker.internet.username() + Math.random(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    updatedAt: Date.now(),
+  };
+
+  await sql`insert into ${sql('User')} ${sql(user)}`;
+
+  return user;
+}
+
 async function main() {
   const tables = ['User', 'OAuth', 'Folder', 'Note', 'Share'];
   await sql`truncate ${sql(tables)}`;
 
-  const user = await createUser();
-  const rootFolder = await createRootFolder(user);
-  await createNotes(user, rootFolder);
+  const promises = [];
+  for (let i = 0; i < 1000; i++) {
+    promises.push((async () => {
+      const user = await createRandomUser();
+      const rootFolder = await createRootFolder(user);
+      await createNotes(user, rootFolder);
+      console.log('inserted: ', i);
+    })());
+  }
+
+  await Promise.all(promises);
+
+  // const user = await createUser();
+  // const rootFolder = await createRootFolder(user);
+  // await createNotes(user, rootFolder);
 }
 
 main().then(() => process.exit());
