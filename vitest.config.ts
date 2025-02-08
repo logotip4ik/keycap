d080ea8b7f1ca2510660f038874c710913fc4f2e
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2025-02-08 18:43:14 +0200

    chore: maybe fix flacky tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,19 @@ import { defaultExclude } from 'vitest/config';
 
 export default defineVitestConfig({
   assetsInclude: ['**.html'],
+
   test: {
     mockReset: true,
     globals: true,
 
     root: './',
+
+    poolOptions: {
+      threads: {
+        maxThreads: 1,
+        minThreads: 1,
+      }
+    },
 
     setupFiles: [
       './server/test/stub-nitro.ts',
