0a64a2d95c62b57514229f2981dad51eeec72f40
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2025-06-07 14:15:57 +0300

    chore: use const instead of getter function

--- server/utils/passwords.ts
+++ server/utils/passwords.ts
@@ -1,21 +1,18 @@
 import argon2 from '@node-rs/argon2';
 import bcrypt from '@node-rs/bcrypt';
 
-function getArgon2Options(): argon2.Options {
-  // https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#introduction
-  return {
-    timeCost: 2,
-    memoryCost: 32768, // 32mb
-  };
-}
+const argon2Options: argon2.Options = {
+  timeCost: 2,
+  memoryCost: 32768, // 32mb
+};
 
 export function hashPassword(pass: string): Promise<string> {
-  return argon2.hash(pass, getArgon2Options());
+  return argon2.hash(pass, argon2Options);
 }
 
 export function verifyPassword(hashedPass: string, pass: string): Promise<boolean> {
   if (hashedPass.startsWith('$argon2')) {
-    return argon2.verify(hashedPass, pass, getArgon2Options());
+    return argon2.verify(hashedPass, pass, argon2Options);
   }
 
   return bcrypt.compare(pass, hashedPass);
