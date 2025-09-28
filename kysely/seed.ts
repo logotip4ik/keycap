4dc1f3db00c743411b79ba94cd3775f16a0f0947
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2025-09-28 15:26:17 +0300

    refactor: update seed script

--- kysely/seed.ts
+++ kysely/seed.ts
@@ -2,6 +2,7 @@ import { fakerEN as faker } from '@faker-js/faker';
 import postgres from 'postgres';
 import { read } from 'rc9';
 
+import type { NewFolder, NewNote, NewUser } from './db/types';
 import { hashPassword } from '../server/utils/passwords';
 
 const url: string = read('.env').DATABASE_URL;
@@ -13,82 +14,88 @@ if (!url) {
 const sql = postgres(url);
 
 async function createUser() {
-  const defaultUser = {
-    id: '1',
+  const defaultUser: NewUser = {
     username: 'bogdankostyuk',
     email: 'contact@bogdankostyuk.xyz',
     password: await hashPassword('something'),
-    updatedAt: Date.now(),
+    updatedAt: new Date(),
   };
 
-  await sql`insert into ${sql('User')} ${sql(defaultUser)}`;
+  const [{ id }] = await sql`insert into ${sql('User')} ${sql(defaultUser)} returning ${sql(['id'])}`;
 
-  return defaultUser;
+  return {
+    ...defaultUser,
+    id,
+  };
 }
 
 async function createRootFolder(user: { id: string, username: string }) {
-  const rootFolder = {
-    id: faker.number.bigInt().toString(),
+  const rootFolder: NewFolder = {
     name: `${user.username}'s workspace`,
     path: `/${user.username}`,
     root: true,
     ownerId: user.id,
-    updatedAt: Date.now(),
+    updatedAt: new Date(),
   };
 
-  await sql`insert into ${sql('Folder')} ${sql(rootFolder)}`;
+  const [{ id }] = await sql`insert into ${sql('Folder')} ${sql(rootFolder)} returning ${sql(['id'])}`;
 
-  return rootFolder;
+  return {
+    ...rootFolder,
+    id
+  };
 }
 
 async function createNotes(user: { id: string }, folder: { id: string, path: string }) {
   const notesNames = ['main', 'test', 'updates', 'chore'];
 
-  const notes = notesNames.map((name) => ({
+  const notes = notesNames.map((name): NewNote => ({
     name,
     content: `<p>${faker.lorem.sentences(15)}</p>`,
     path: `${folder.path}/${name}`,
     ownerId: user.id,
     parentId: folder.id,
-    updatedAt: Date.now(),
+    updatedAt: new Date(),
   }));
 
   await sql`insert into ${sql('Note')} ${sql(notes)}`;
 }
 
 async function createRandomUser() {
-  const user = {
-    id: faker.number.bigInt().toString(),
+  const user: NewUser = {
     username: faker.internet.username() + Math.random(),
     email: faker.internet.email(),
     password: faker.internet.password(),
-    updatedAt: Date.now(),
+    updatedAt: new Date(),
   };
 
-  await sql`insert into ${sql('User')} ${sql(user)}`;
+  const [{ id }] = await sql`insert into ${sql('User')} ${sql(user)} returning ${sql(['id'])}`;
 
-  return user;
+  return {
+    ...user,
+    id,
+  };
 }
 
 async function main() {
   const tables = ['User', 'OAuth', 'Folder', 'Note', 'Share'];
   await sql`truncate ${sql(tables)}`;
 
-  const promises = [];
+  const promises: Array<Promise<void>> = [];
   for (let i = 0; i < 1000; i++) {
     promises.push((async () => {
       const user = await createRandomUser();
       const rootFolder = await createRootFolder(user);
       await createNotes(user, rootFolder);
-      console.log('inserted: ', i);
+      // console.log('inserted: ', i);
     })());
   }
 
   await Promise.all(promises);
 
-  // const user = await createUser();
-  // const rootFolder = await createRootFolder(user);
-  // await createNotes(user, rootFolder);
+  const user = await createUser();
+  const rootFolder = await createRootFolder(user);
+  await createNotes(user, rootFolder);
 }
 
 main().then(() => process.exit());
